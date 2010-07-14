b797bc44563ee0603f220de5d609d7e2b9e3bf8d
jiaoyuhai <jiaoyuhai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-07-14 06:43:43 +0000

    For MP3
    git-svn-id: svn://127.0.0.1@675 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/build/ms/custw204.h
+++ main/build/ms/custw204.h
@@ -226,6 +226,7 @@ Copyright (c) 2001-2010 by QUALCOMM Incorporated.  All Rights Reserved.
 #define FEATURE_DRV_SDCC
 #define FEATURE_SDCC_CLK_CONFIG
 #define FEATURE_HS_USB_MS_FD
+#define FEATURE_AUDIO_CAMERA_CONCURRENCY // FOR MP3
 #endif
 
 #include "custuim.h"
