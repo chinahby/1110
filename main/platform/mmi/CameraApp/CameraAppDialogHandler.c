ca41b01ebd3c59d87c8d7c8af07d91872a2ecdff
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-07-31 06:50:49 +0000

    git-svn-id: svn://127.0.0.1@936 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CameraApp/CameraAppDialogHandler.c
+++ main/platform/mmi/CameraApp/CameraAppDialogHandler.c
@@ -374,6 +374,7 @@ static boolean CameraApp_MainMenuHandleEvent(CCameraApp *pMe, AEEEvent eCode, ui
     PARAM_NOT_REF(dwParam)
         
     IMenuCtl *pMenu = (IMenuCtl*)IDIALOG_GetControl(pMe->m_pActiveDlg, IDC_CAMERA_MAINMENU);
+	AECHAR WTitle[40] = {0};
    
     if(pMenu == NULL)
     {
@@ -388,7 +389,19 @@ static boolean CameraApp_MainMenuHandleEvent(CCameraApp *pMe, AEEEvent eCode, ui
             
             pMe->m_wMsgID = IDS_MSG_WAITING;
             pMe->m_nMsgTimeout = TIMEOUT_MS_MSGBOX;
-
+			#ifdef FEATURE_VERSION_IVIO
+				(void)ISHELL_LoadResString(pMe->m_pShell,
+                                    AEE_APPSCAMERAAPP_RES_FILE,                                
+                                    IDS_ITEM_CAMERA,
+                                    WTitle,
+                                    sizeof(WTitle));
+				if(pMe->m_pIAnn != NULL)
+            	{
+			    	IANNUNCIATOR_SetFieldText(pMe->m_pIAnn,WTitle);
+				}
+		
+			#endif
+            
             IDISPLAY_SetClipRect(pMe->m_pDisplay, NULL); 
 
             IANNUNCIATOR_EnableAnnunciatorBar(pMe->m_pIAnn,AEECLSID_DISPLAY1,TRUE);
