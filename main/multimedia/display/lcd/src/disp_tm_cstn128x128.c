649212d8e5a1cacd5e86a5fe005ff2c2e0bbb7f9
xuhui <xuhui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-08-06 03:29:07 +0000

    git-svn-id: svn://127.0.0.1@1043 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/multimedia/display/lcd/src/disp_tm_cstn128x128.c
+++ main/multimedia/display/lcd/src/disp_tm_cstn128x128.c
@@ -651,14 +651,14 @@ static void tm_cstn128x128_disp_powerup(void)
 		//////////////////////////////////////////////////////
     	/////VOP set///////////
     	TM_SCTN_WRITE_CMD(0xc0);  //vo voltage set   
-    	TM_SCTN_WRITE_DATA(0x1f);//////////ff
-    	TM_SCTN_WRITE_DATA(0x01);//00//01
+    	TM_SCTN_WRITE_DATA(0xfb);//////////ff
+    	TM_SCTN_WRITE_DATA(0x00);//00//01
 
         TM_SCTN_WRITE_CMD(0x25);   //write contrast for mobile
     	TM_SCTN_WRITE_DATA(0x3f);
     	//-----------OTPB SET----------------------//
     	TM_SCTN_WRITE_CMD(0xc3);// bias set
-    	TM_SCTN_WRITE_DATA(0x01);    //1/12
+    	TM_SCTN_WRITE_DATA(0x00);    //1/12
     	TM_SCTN_WRITE_CMD(0xc4);   //booster set
     	TM_SCTN_WRITE_DATA(0x06);//  //06   7±¶Ñ¹
     	TM_SCTN_WRITE_CMD(0xc5);   //booster efficiency set
