d8bdbae955f4d18c9640594138b39e6b7f1454a5
jiaoyuhai <jiaoyuhai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-08-14 02:05:23 +0000

    git-svn-id: svn://127.0.0.1@1246 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CoreApp/CoreAppStateHandler.c
+++ main/platform/mmi/CoreApp/CoreAppStateHandler.c
@@ -703,7 +703,7 @@ static NextFSMAction COREST_VERIFYUIM_Handler(CCoreApp *pMe)
                         // 通过验证
                         //MOVE_TO_STATE(COREST_POWERONSYSINIT)
                         MOVE_TO_STATE(COREST_STARTUPANI);
-                        pMe->m_eUIMErrCode == UIMERR_NONE
+                        pMe->m_eUIMErrCode == UIMERR_NONE;
                         CoreApp_ProcessSubscriptionStatus(pMe);
                     }
                     else
@@ -754,7 +754,7 @@ static NextFSMAction COREST_VERIFYUIM_Handler(CCoreApp *pMe)
                         // 成功解锁
                         //MOVE_TO_STATE(COREST_POWERONSYSINIT)
                         MOVE_TO_STATE(COREST_STARTUPANI);
-                        pMe->m_eUIMErrCode == UIMERR_NONE
+                        pMe->m_eUIMErrCode == UIMERR_NONE;
                         CoreApp_ProcessSubscriptionStatus(pMe);
                     }
                     else
