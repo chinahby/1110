0fa8c8be08fa61ed1708e5cc13dac3da200bf2c5
jiaoyuhai <jiaoyuhai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-08-17 23:19:31 +0000

    git-svn-id: svn://127.0.0.1@1298 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

---
+++ tags/V0.4-0817/wconnect/bthost/pf/src/btpfhidif.h
@@ -0,0 +1,10 @@
+#ifndef _BTPFHIDIF_H
+#define _BTPFHIDIF_H
+
+#ifdef FEATURE_BT
+#ifdef FEATURE_BT_EXTPF_HID_HOST
+#error code not present
+#endif /* FEATURE_BT_EXTPF_HID_HOST */
+#endif /* FEATURE_BT */
+#endif /* _BTPFHIDIF_H */
+
