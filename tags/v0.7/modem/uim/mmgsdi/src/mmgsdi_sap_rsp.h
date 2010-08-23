79182068d6708ac4cfe9406c7737b251fef68629
pengyuangui <pengyuangui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-08-23 11:15:37 +0000

    git-svn-id: svn://127.0.0.1@1334 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

---
+++ tags/v0.7/modem/uim/mmgsdi/src/mmgsdi_sap_rsp.h
@@ -0,0 +1,4 @@
+#if (defined(FEATURE_BT) || defined(FEATURE_WM_BT)) && defined (FEATURE_BT_EXTPF_SAP)
+#error code not present
+#endif /* (defined(FEATURE_BT) || defined(FEATURE_WM_BT)) && defined (FEATURE_BT_EXTPF_SAP) */
+
