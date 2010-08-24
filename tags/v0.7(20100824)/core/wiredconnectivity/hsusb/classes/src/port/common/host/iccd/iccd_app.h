289aaf43d588f8137637cfa1cae782096bf18bd4
pengyuangui <pengyuangui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-08-24 11:09:11 +0000

    20100824 V0.7
    git-svn-id: svn://127.0.0.1@1346 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

---
+++ tags/v0.7(20100824)/core/wiredconnectivity/hsusb/classes/src/port/common/host/iccd/iccd_app.h
@@ -0,0 +1,9 @@
+/* Jungo Confidential, Copyright (c) 2008 Jungo Ltd.  http://www.jungo.com */
+/* OS Porting Layer HID Class Driver API */
+
+#ifndef _HOST_ICCD_APP
+#define _HOST_ICCD_APP
+
+
+#endif
+
