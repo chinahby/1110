3888f39aeb3c7a4664bfbe71abb9c8e21f0a62d0
xuhui <xuhui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-08-30 07:56:29 +0000

    git-svn-id: svn://127.0.0.1@1399 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/build/ms/custsmart.h
+++ main/build/ms/custsmart.h
@@ -219,7 +219,6 @@ Copyright (c) 2001-2010 by QUALCOMM Incorporated.  All Rights Reserved.
 #define FEATURE_DUAL_UIMCARD
 #define FEATRUE_AUTO_SET_NEED_NV_VALUE
 #define FEATURE_FM_RADIO
-#define FEATURE_CAMERA_SUPPORT  //add by xuhui
 #define FEATURE_FM_OEM
 #define FEATURE_PROJECT_SMART
 #define FEATURE_PROJECT_W204
