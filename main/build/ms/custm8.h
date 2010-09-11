61790cdbb522ea91be1beeaa2d60c0e65055f763
wangliang <wangliang@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-09-11 05:29:59 +0000

    去掉和SMARTFREN不想关的宏
    git-svn-id: svn://127.0.0.1@1555 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/build/ms/custm8.h
+++ main/build/ms/custm8.h
@@ -236,9 +236,6 @@ Copyright (c) 2001-2010 by QUALCOMM Incorporated.  All Rights Reserved.
 #define FEATURE_AUDIO_CAMERA_CONCURRENCY // FOR MP3
 #define FEATURE_FRENDUO //Add By zzg 2010_08_23 for Frenduo
 
-#define FEATURE_FPT005 //add by xuhui
-#define FEATURE_CAH006 //add by xuhui
-#define FEATURE_FMN2010//add by xuhui
 #endif
 
 #include "custuim.h"
