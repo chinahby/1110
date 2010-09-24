75c33bb5e407bd7231fdbb2094e4394ffc1acbac
wangliang <wangliang@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-09-24 12:36:07 +0000

    4GV模式下MIC声音的调整
    git-svn-id: svn://127.0.0.1@1671 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/multimedia/audio/avs/src/voccal.c
+++ main/multimedia/audio/avs/src/voccal.c
@@ -11069,10 +11069,17 @@ CAL_MEMORY voc_pcm_path_cal_type voc_pcm_on_chip_0_cal_wb = {
       defined(MSMAUD_QSC1110_AUDIO_CAL)
   VOC_EC_ESEC,                   /* Echo Canceller mode     */
   VOC_NS_ON,                     /* Noise suppressor enable */
+#ifdef CUST_EDITION
+  0x956F,						 /* Tx Voice Volume 		*/
+  0x2000,						 /* Tx DTMF gain			*/
+  0x956F,						 /* CODEC Tx gain			*/
+  0x71CF,						 /* CODEC Rx gain			*/
+#else
   0x4000,                        /* Tx Voice Volume         */
   0x1000,                        /* Tx DTMF gain            */
   0x1FD9,                        /* CODEC Tx gain           */
   0x4000,                        /* CODEC Rx gain           */
+#endif
   0x400,                         /* CODEC sidetone gain     */
 #ifdef MSMAUD_PCM_DC_OFFSET
   0xFF18,                        /* CODEC Rx left chan offset */
@@ -11681,11 +11688,19 @@ CAL_MEMORY voc_pcm_path_cal_type voc_pcm_off_chip_dual_mic_cal = {
       defined(MSMAUD_QSC1110_AUDIO_CAL)
   VOC_EC_ESEC,                   /* Echo Canceller mode     */
   VOC_NS_ON,                     /* Noise suppressor enable */
+#ifdef CUST_EDITION
+  0x956F,						 /* Tx Voice Volume 		*/
+  0x2000, 					   /* Tx DTMF gain			  */
+  0x956F, 					   /* CODEC Tx gain 		  */
+  0x71CF, 					   /* CODEC Rx gain 		  */
+  0x800,                         /* CODEC sidetone gain     */
+#else
   0x4000,                        /* Tx Voice Volume         */
   0x1000,                        /* Tx DTMF gain            */
   0x1FD9,                        /* CODEC Tx gain           */
   0x4000,                        /* CODEC Rx gain           */
   0x400,                         /* CODEC sidetone gain     */
+#endif
 #ifdef MSMAUD_PCM_DC_OFFSET
   0xFF18,                        /* CODEC Rx left chan offset */
   0xFEEA,                        /* CODEC Rx right chan offset */
