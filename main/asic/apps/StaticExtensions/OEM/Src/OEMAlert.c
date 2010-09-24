95b5bd0825c5fde9716657332633fb3be294a516
wangliang <wangliang@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-09-24 11:59:55 +0000

    git-svn-id: svn://127.0.0.1@1669 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/asic/apps/StaticExtensions/OEM/Src/OEMAlert.c
+++ main/asic/apps/StaticExtensions/OEM/Src/OEMAlert.c
@@ -2244,6 +2244,11 @@ static void OEMALERT_StartRingerAlert(IALERT *pMe,uint32 id,ALERT_SND_TYPE type)
     if(type == ALERT_POWER_SND)
     {
         alerttype = OEMNV_ALERTTYPE_RINGER;
+    //wangliang add! 2010-09-24
+    #if defined(FEATURE_PROJECT_SMART) || defined(FEATURE_PROJECT_M8)
+        pMe->m_ringCurVol = OEMSOUND_2ND_VOL;
+        pMe->m_ringEndVol = OEMSOUND_2ND_VOL;
+    #endif
     }
 #ifdef FEATURE_SMSTONETYPE_MID      
     else if(type == ALERT_SMS_SND)
