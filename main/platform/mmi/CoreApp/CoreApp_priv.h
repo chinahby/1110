c01dfdaf39b09a6edcb8ae5f4f77bdcd1184b5f8
xuhui <xuhui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-10-16 02:49:09 +0000

    百立丰H19C开关机动画铃声待机图片修改
    git-svn-id: svn://127.0.0.1@1933 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CoreApp/CoreApp_priv.h
+++ main/platform/mmi/CoreApp/CoreApp_priv.h
@@ -175,10 +175,10 @@
 #define    ANI_RATE                              (150)
 #elif defined FEATURE_VERSION_H19C
 #define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poon.gif"
-#define    PWRON_ANI_FRAME_COUNT                 (21)
+#define    PWRON_ANI_FRAME_COUNT                 (12)
 #define    PWRON_ANI_RATE                        (150)
 #define    PWROFF_ANI_FILE                       "fs:/image/pwronoffani/poon.gif"
-#define    PWROFF_ANI_FRAME_COUNT                (21)
+#define    PWROFF_ANI_FRAME_COUNT                (12)
 #define    PWROFF_ANI_RATE                       (380)
 #define    ANI_RATE                              (150)
 #elif defined FEATURE_VERSION_ITEL
