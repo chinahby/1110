1e0f3436ef5e873aec1a65ae74fe7c33226ab5b4
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-11-15 08:27:28 +0000

    git-svn-id: svn://127.0.0.1@2232 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CoreApp/CoreApp_priv.h
+++ main/platform/mmi/CoreApp/CoreApp_priv.h
@@ -124,12 +124,12 @@
 // 关机动画文件播放
 #ifdef FEATURE_VERSION_FLEXI203
 #define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweronani.png"
-#define    PWRON_ANI_FRAME_COUNT                 (19)
-#define    PWRON_ANI_RATE                        (200)
+#define    PWRON_ANI_FRAME_COUNT                 (17)
+#define    PWRON_ANI_RATE                        (280)
 #define    PWROFF_ANI_FILE                       "fs:/image/pwronoffani/poweronani.png"
-#define    PWROFF_ANI_FRAME_COUNT                (19)
-#define    PWROFF_ANI_RATE                       (200)
-#define    ANI_RATE                              (150)
+#define    PWROFF_ANI_FRAME_COUNT                (17)
+#define    PWROFF_ANI_RATE                       (280)
+#define    ANI_RATE                              (180)
 #elif defined FEATURE_VERSION_SMART
 #define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweronani.png"
 #define    PWRON_ANI_FRAME_COUNT                 (10)
