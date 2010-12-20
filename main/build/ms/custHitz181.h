c5fa0c727b98aa47ea3d3ddd3067b0280d0d3247
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-12-20 06:21:57 +0000

    git-svn-id: svn://127.0.0.1@2689 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/build/ms/custHitz181.h
+++ main/build/ms/custHitz181.h
@@ -252,7 +252,7 @@ Copyright (c) 2001-2010 by QUALCOMM Incorporated.  All Rights Reserved.
 #define FEATURE_SDCC_CLK_CONFIG
 #define FEATURE_HS_USB_MS_FD
 #define FEATURE_AUDIO_CAMERA_CONCURRENCY // FOR MP3
-#define FEATURE_FRENDUO //Add By zzg 2010_08_23 for Frenduo
+//#define FEATURE_FRENDUO //Add By zzg 2010_08_23 for Frenduo
 //#define FEATURE_AAC
 //#define FEATURE_AAC_PLUS
 //#define FEATURE_ENHANCED_AAC_PLUS
