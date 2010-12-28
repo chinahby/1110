5e0a91311b0291b6a269aa534f918f97e1b85452
zhuzhenguo <zhuzhenguo@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-12-28 01:19:28 +0000

    git-svn-id: svn://127.0.0.1@2747 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- temp/modem/uim/mmgsdi/src/mmgsdi_sap_rsp.h
+++
@@ -1,4 +0,0 @@
-#if (defined(FEATURE_BT) || defined(FEATURE_WM_BT)) && defined (FEATURE_BT_EXTPF_SAP)
-#error code not present
-#endif /* (defined(FEATURE_BT) || defined(FEATURE_WM_BT)) && defined (FEATURE_BT_EXTPF_SAP) */
-
--- temp/tools/headergen/buildpartition.cmd
+++
@@ -1,7 +0,0 @@
-@if exist partition.mbn rm partition.mbn
-@make -f partition.mak %1
-@echo ------------------------------------------------------------
-@echo partition.mbn is Generated.
-@echo ------------------------------------------------------------
-
-
