118f02b18cb676ec2f789c141984a81acca58fe4
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2010-12-28 02:03:29 +0000

    git-svn-id: svn://127.0.0.1@2753 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

---
+++ flexi203guru/build/ms/clean.bat
@@ -0,0 +1,11 @@
+del /s /q ..\..\*.o
+del /s /q ..\..\*.d
+del /s /q ..\..\*.lst
+del /s /q ..\..\*.si
+del /s /q ..\..\*.lib
+del /s /q ..\..\*.log
+del /s /q ..\..\*.?~
+del /s /q ..\..\*.??~
+del /s /q ..\..\*.???~
+del /s /q ..\..\*.????~
+del /q warning.txt
