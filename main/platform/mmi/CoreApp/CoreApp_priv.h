c6b20efe9592898b8f2aeee39e3f3b8507feb1be
pengyuangui <pengyuangui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-01-17 08:05:39 +0000

    H19C
    git-svn-id: svn://127.0.0.1@3053 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CoreApp/CoreApp_priv.h
+++ main/platform/mmi/CoreApp/CoreApp_priv.h
@@ -227,7 +227,7 @@
 #define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poon.gif"
 #define    PWRON_ANI_FRAME_COUNT                 (1)
 #define    PWRON_ANI_RATE                        (5000)
-#define    PWROFF_ANI_FILE                       PWRON_ANI_FILE
+#define    PWROFF_ANI_FILE                       "fs:/image/pwronoffani/pooff.gif"
 #define    PWROFF_ANI_FRAME_COUNT                PWRON_ANI_FRAME_COUNT
 #define    PWROFF_ANI_RATE                       PWRON_ANI_RATE
 #elif defined FEATURE_VERSION_ITEL
