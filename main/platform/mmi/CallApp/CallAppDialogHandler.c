6e6ce6ee4bc6fb89e1cc134cc52b52b0eda72e0a
pengyuangui <pengyuangui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-01-18 08:53:04 +0000

    任意键接听 增加 AVK_CAMERA  AVK_MUSIC
    git-svn-id: svn://127.0.0.1@3073 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CallApp/CallAppDialogHandler.c
+++ main/platform/mmi/CallApp/CallAppDialogHandler.c
@@ -7120,7 +7120,6 @@ boolean CallApp_AnswerCall(CCallApp  *pMe, boolean bAnswerHold,AEEEvent eCode,ui
         return FALSE;
     }
 
-
     (void) ICONFIG_GetItem(pMe->m_pConfig,
                                         CFGI_KEYGUARD_ENABLED,/*it can not use in current,because oemcfig not process, p1113*/
                                         &bKeyguardEnabled,
@@ -7132,15 +7131,15 @@ boolean CallApp_AnswerCall(CCallApp  *pMe, boolean bAnswerHold,AEEEvent eCode,ui
 	OEMNV_Get(NV_SIM_SELECT_I,&SimChoice);
 	if(SimChoice.sim_select==AVK_SEND_TWO)
 	{
-		 if((((wParam == AVK_SEND || wParam == AVK_CAMERA) && (pMe->m_anykey_answer & 0x4))
+		 if((((wParam == AVK_SEND || wParam == AVK_CAMERA || wParam == AVK_MUSIC) && (pMe->m_anykey_answer & 0x4))
         ||(eCode == EVT_FLIP && ((boolean)wParam == TRUE)  && (pMe->m_anykey_answer & 0x2))
-        ||(((wParam == AVK_USER_HEADSET) || (wParam == AVK_SEND)|| wParam == AVK_CAMERA) && (pMe->m_anykey_answer & 0x8))
+        ||(((wParam == AVK_USER_HEADSET) || (wParam == AVK_SEND)|| (wParam == AVK_CAMERA) || (wParam == AVK_MUSIC)) && (pMe->m_anykey_answer & 0x8))
         ||(((((AVK_FIRST < wParam && wParam <AVK_POWER ) ||(wParam == AVK_INFO)||(wParam == AVK_SHIFT)||
         	   (wParam == AVK_ENTER)||(wParam == AVK_CAPLK)||(wParam == AVK_SYMBOL)||
         	   (wParam == AVK_RWD)||(wParam == AVK_LCTRL)||(wParam == AVK_SPACE)||
         	   (AVK_A <= wParam && wParam <= AVK_Z) ||(AVK_CLR < wParam && wParam <AVK_SOFT1 ))
                  && !bKeyguardEnabled)
-                 ||(wParam == AVK_SEND || wParam == AVK_CAMERA))
+                 ||(wParam == AVK_SEND || wParam == AVK_CAMERA || wParam == AVK_MUSIC))
                  && (pMe->m_anykey_answer & 0x1))
         ) ||auto_answer ||wParam == AVK_SELECT)
     {
@@ -7207,15 +7206,15 @@ boolean CallApp_AnswerCall(CCallApp  *pMe, boolean bAnswerHold,AEEEvent eCode,ui
 	}
 	}
 	#endif
-    if(((wParam == AVK_SEND && (pMe->m_anykey_answer & 0x4))
+    if((((wParam == AVK_SEND|| wParam == AVK_CAMERA || wParam == AVK_MUSIC) && (pMe->m_anykey_answer & 0x4))
         ||(eCode == EVT_FLIP && ((boolean)wParam == TRUE)  && (pMe->m_anykey_answer & 0x2))
-        ||(((wParam == AVK_USER_HEADSET) || (wParam == AVK_SEND)) && (pMe->m_anykey_answer & 0x8))
+        ||(((wParam == AVK_USER_HEADSET) || (wParam == AVK_SEND )|| (wParam == AVK_CAMERA) || (wParam == AVK_MUSIC)) && (pMe->m_anykey_answer & 0x8))
         ||(((((AVK_FIRST < wParam && wParam <AVK_POWER ) ||(wParam == AVK_INFO)||(wParam == AVK_SHIFT)||
         	   (wParam == AVK_ENTER)||(wParam == AVK_CAPLK)||(wParam == AVK_SYMBOL)||
         	   (wParam == AVK_RWD)||(wParam == AVK_LCTRL)||(wParam == AVK_SPACE)||
         	   (AVK_A <= wParam && wParam <= AVK_Z) ||(AVK_CLR < wParam && wParam <AVK_SOFT1 ))
                  && !bKeyguardEnabled)
-                 ||(wParam == AVK_SEND))
+                 ||(wParam == AVK_SEND || wParam == AVK_CAMERA || wParam == AVK_MUSIC))
                  && (pMe->m_anykey_answer & 0x1))
         ) ||auto_answer ||wParam == AVK_SELECT)
     {
