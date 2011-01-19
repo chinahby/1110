2088e51f8952f08800d7c91a1383a9339daedc83
wangliang <wangliang@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-01-19 09:18:52 +0000

    git-svn-id: svn://127.0.0.1@3094 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

---
+++ Hitz233C/platform/brew/brewpk/pk/inc/AEEStockFonts.h
@@ -0,0 +1,9 @@
+#ifndef AEESTOCKFONTS_H
+#define AEESTOCKFONTS_H
+
+#include "AEEModTable.h"
+
+extern const AEEStaticClass gAEEStockFontsClasses[];
+extern const AEEStaticClass gAEEStandardFontsClasses[];
+
+#endif // AEESTOCKFONTS_H
---
+++ Hitz233C/platform/mmi/BlueTooth/btapp_res.h
@@ -0,0 +1,7 @@
+
+#ifndef BTAPP_RES_H
+#define BTAPP_RES_H
+
+#include "btapp.brh"
+
+#endif  // BTAPP_RES_H
