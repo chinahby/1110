01fa21ac6c1a9d11ea462fe824fb98155c0e2296
wangliang <wangliang@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-01-19 09:11:14 +0000

    git-svn-id: svn://127.0.0.1@3087 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

---
+++ Hitz181_M8/wconnect/bthost/pf/src/oi_ver_avrcp.h
@@ -0,0 +1,14 @@
+#ifndef OI_VERSION_AVRCP_H
+#define OI_VERSION_AVRCP_H
+/**
+ *  Module version
+ */
+/**********************************************************************************
+  $Revision: #1 $
+  Copyright 2005 Open Interface North America, Inc. All rights reserved.
+***********************************************************************************/
+
+#define OI_VERSION_AVRCP   7235
+
+/*****************************************************************************/
+#endif /* OI_VERSION_AVRCP_H */
