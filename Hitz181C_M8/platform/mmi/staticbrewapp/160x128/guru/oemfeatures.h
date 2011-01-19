f9a72123dfbaaeeeabff40aa5e19bc7be6b681c4
wangliang <wangliang@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-01-19 09:16:01 +0000

    back up
    git-svn-id: svn://127.0.0.1@3091 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

---
+++ Hitz181C_M8/platform/mmi/staticbrewapp/160x128/guru/oemfeatures.h
@@ -0,0 +1,8 @@
+#ifndef OEMFEATURES_H
+#define OEMFEATURES_H
+#ifdef FEATURE_APP_GURU
+
+#define AEECLSID_FLEXIGURU    	0x0109a9cd
+
+#endif
+#endif   // OEMFEATURES_H
---
+++ Hitz181C_M8/wconnect/bthost/brew/statext/src/OEMBTExtSAP.h
@@ -0,0 +1,10 @@
+#ifndef _OEMBTEXTSAP_H_
+#define _OEMBTEXTSAP_H_
+
+#if defined(FEATURE_IBT) && \
+    defined(FEATURE_BT_EXTPF_SAP) && defined(FEATURE_BT_TEST_API)
+#error code not present
+#endif /* FEATURE_IBT && FEATURE_BT_EXTPF_SAP && FEATURE_BT_TEST_API */
+
+#endif // _OEMBTEXTSAP_H_
+
