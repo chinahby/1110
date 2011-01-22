5d06a37df6e9f2b0baa115044dfba1fe0fc8c353
miaoxiaoming <miaoxiaoming@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-01-22 02:14:55 +0000

    S5000备份
    git-svn-id: svn://127.0.0.1@3132 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

---
+++ S5000/core/systemdrivers/hw/src/buildhwio.bat
@@ -0,0 +1,2 @@
+perl msmreg.pl QSC1100_CAF_v1p46.FLAT -omHWIO -h=msmhwioreg.h -shadow=msmshadow -jaguar -i=msmreg_input_proprietary.dat -forceshadow -xref
+
---
+++ S5000/platform/mmi/staticbrewapp/220x176/musik/oemfeatures.h
@@ -0,0 +1,6 @@
+#ifndef OEMFEATURES_H
+#define OEMFEATURES_H
+
+#define AEECLSID_FGEN    	0x01009279
+
+#endif   // OEMFEATURES_H
