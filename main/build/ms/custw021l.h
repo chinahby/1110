6546b9a86b6d03b7c0e8a78578602b89e0ac8c50
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-02-21 03:17:12 +0000

    git-svn-id: svn://127.0.0.1@3285 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/build/ms/custw021l.h
+++ main/build/ms/custw021l.h
@@ -216,7 +216,7 @@ Copyright (c) 2001-2010 by QUALCOMM Incorporated.  All Rights Reserved.
 #define FEATURE_XO 
 
 #ifdef CUST_EDITION
-#define FEATURE_DUAL_UIMCARD
+//#define FEATURE_DUAL_UIMCARD
 #define FEATRUE_AUTO_SET_NEED_NV_VALUE
 //#ifndef FEATURE_USES_LOWMEM
 #define FEATURE_FM_RADIO
