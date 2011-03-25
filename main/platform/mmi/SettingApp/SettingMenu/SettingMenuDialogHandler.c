54296740d3e822d84904b8d28b7c3fa5d1b91503
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-03-25 11:37:01 +0000

    git-svn-id: svn://127.0.0.1@3512 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/SettingApp/SettingMenu/SettingMenuDialogHandler.c
+++ main/platform/mmi/SettingApp/SettingMenu/SettingMenuDialogHandler.c
@@ -3939,7 +3939,7 @@ static boolean  HandleLanguageDialogEvent(CSettingMenu *pMe,
                 case IDS_THAI:    //Ì©¹úÓï
                     language = NV_LANGUAGE_THAI;
                     #ifdef FEATURE_T9_RAPID_THAI
-                    inputmode = OEM_MODE_RAPID_MT_THAI;   
+                    inputmode = OEM_MODE_T9_MT_THAI;   
                     #else
                     inputmode = OEM_MODE_T9_MT_THAI;
                     #endif
