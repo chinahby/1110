87eb4355fb2163c50abafa939b42a6f4fb0ecec3
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-05-17 07:24:01 +0000

    N450
    git-svn-id: svn://127.0.0.1@3688 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CoreApp/CoreApp_priv.h
+++ main/platform/mmi/CoreApp/CoreApp_priv.h
@@ -320,7 +320,13 @@
 #define    PWROFF_ANI_FILE                       "fs:/image/pwronoffani/poweroff.gif"
 #define    PWROFF_ANI_FRAME_COUNT                PWRON_ANI_FRAME_COUNT
 #define    PWROFF_ANI_RATE                       PWRON_ANI_RATE
-
+#elif defined FEAUTRE_VERSION_N450
+#define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweron.gif"
+#define    PWRON_ANI_FRAME_COUNT                 (1)
+#define    PWRON_ANI_RATE                        (4000)
+#define    PWROFF_ANI_FILE                       "fs:/image/pwronoffani/poweroff.gif"
+#define    PWROFF_ANI_FRAME_COUNT                PWRON_ANI_FRAME_COUNT
+#define    PWROFF_ANI_RATE                       PWRON_ANI_RATE
 #else
 #define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweronoff.gif"
 #define    PWRON_ANI_FRAME_COUNT                 (1)
