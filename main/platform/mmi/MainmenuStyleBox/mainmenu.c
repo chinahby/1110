df8875bd5457f6582f24f1abde3b312830c59860
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-05-30 08:08:02 +0000

    git-svn-id: svn://127.0.0.1@3718 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/MainmenuStyleBox/mainmenu.c
+++ main/platform/mmi/MainmenuStyleBox/mainmenu.c
@@ -623,15 +623,15 @@ static int CMainMenu_InitAppData(MainMenu *pMe)
     pMe->m_IconTitle[0]     = IDS_MAIN_MENU_MEDIAGALLERY;
     pMe->m_IconTitle[1]     = IDS_MAIN_MENU_CONTACTS;
     pMe->m_IconTitle[2]     = IDS_MAIN_MENU_UTK;
-    pMe->m_IconTitle[3]     = IDS_MAIN_MENU_MULTIMEDIA;
-    pMe->m_IconTitle[4]     = IDS_MAIN_MENU_MESSAGES;
-    pMe->m_IconTitle[5]     = IDS_MAIN_MENU_RECENTCALLS;
-    pMe->m_IconTitle[6]     = IDS_MAIN_MENU_MUSICPLAYER;
-    pMe->m_IconTitle[7]     = IDS_MAIN_MENU_SETTINGS;
-    pMe->m_IconTitle[8]     = IDS_MAIN_MENU_APPLICATION;
-    pMe->m_IconTitle[9]     = IDS_MAIN_MENU_GAMES;
-    pMe->m_IconTitle[10]    = IDS_MAIN_MENU_SCHEDULER;
-    pMe->m_IconTitle[11]    = IDS_MAIN_MENU_CALCULATOR;
+    pMe->m_IconTitle[3]     = IDS_MAIN_MENU_CALCULATOR;
+    pMe->m_IconTitle[4]     = IDS_MAIN_MENU_RECENTCALLS;
+    pMe->m_IconTitle[5]     = IDS_MAIN_MENU_MESSAGES;
+    pMe->m_IconTitle[6]     = IDS_MAIN_MENU_MULTIMEDIA;
+    pMe->m_IconTitle[7]     = IDS_MAIN_MENU_APPLICATION;
+    pMe->m_IconTitle[8]     = IDS_MAIN_MENU_SCHEDULER;
+    pMe->m_IconTitle[9]     = IDS_MAIN_MENU_SETTINGS;
+    pMe->m_IconTitle[10]    = IDS_MAIN_MENU_GAMES;
+    pMe->m_IconTitle[11]    = IDS_MAIN_MENU_MUSICPLAYER;
 #endif
 #elif defined (FEATURE_DISP_128X128)
 #ifndef FEATURE_VERSION_H19C
