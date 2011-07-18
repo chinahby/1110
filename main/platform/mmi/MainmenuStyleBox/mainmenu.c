3474109565cff982b67ce9b61d261af9e33b8270
jiaoyuhai <jiaoyuhai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-07-18 07:18:42 +0000

    修正220x176分辨率下主菜单和图标错乱的问题
    git-svn-id: svn://127.0.0.1@3899 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/MainmenuStyleBox/mainmenu.c
+++ main/platform/mmi/MainmenuStyleBox/mainmenu.c
@@ -628,15 +628,15 @@ static int CMainMenu_InitAppData(MainMenu *pMe)
     pMe->m_IconTitle[0]     = IDS_MAIN_MENU_MEDIAGALLERY;
     pMe->m_IconTitle[1]     = IDS_MAIN_MENU_CONTACTS;
     pMe->m_IconTitle[2]     = IDS_MAIN_MENU_UTK;
-    pMe->m_IconTitle[3]     = IDS_MAIN_MENU_CALCULATOR;
-    pMe->m_IconTitle[4]     = IDS_MAIN_MENU_RECENTCALLS;
-    pMe->m_IconTitle[5]     = IDS_MAIN_MENU_MESSAGES;
-    pMe->m_IconTitle[6]     = IDS_MAIN_MENU_MULTIMEDIA;
-    pMe->m_IconTitle[7]     = IDS_MAIN_MENU_APPLICATION;
-    pMe->m_IconTitle[8]     = IDS_MAIN_MENU_SCHEDULER;
-    pMe->m_IconTitle[9]     = IDS_MAIN_MENU_SETTINGS;
-    pMe->m_IconTitle[10]    = IDS_MAIN_MENU_GAMES;
-    pMe->m_IconTitle[11]    = IDS_MAIN_MENU_MUSICPLAYER;
+    pMe->m_IconTitle[3]     = IDS_MAIN_MENU_MULTIMEDIA;
+    pMe->m_IconTitle[4]     = IDS_MAIN_MENU_MESSAGES;
+    pMe->m_IconTitle[5]     = IDS_MAIN_MENU_RECENTCALLS;
+    pMe->m_IconTitle[6]     = IDS_MAIN_MENU_MUSICPLAYER;
+    pMe->m_IconTitle[7]     = IDS_MAIN_MENU_SETTINGS;
+    pMe->m_IconTitle[8]     = IDS_MAIN_MENU_APPLICATION;
+    pMe->m_IconTitle[9]     = IDS_MAIN_MENU_GAMES;
+    pMe->m_IconTitle[10]    = IDS_MAIN_MENU_SCHEDULER;
+    pMe->m_IconTitle[11]    = IDS_MAIN_MENU_CALCULATOR;
 #endif
 #elif defined (FEATURE_DISP_128X128)
 #ifndef FEATURE_VERSION_H19C
@@ -1924,10 +1924,10 @@ static int StartApplet(MainMenu *pMe, int i)
     case IDS_MAIN_MENU_GAMES:
         Result = ISHELL_StartApplet(pMe->m_pShell, AEECLSID_GAME);
         break;
+        
     case IDS_MAIN_MENU_CALCULATOR:
 		 Result = ISHELL_StartApplet(pMe->m_pShell, AEECLSID_CALCAPP);
         break;
-
         
     case IDS_MAIN_MENU_FMRADIO:
         Result = ISHELL_StartApplet(pMe->m_pShell, AEECLSID_APP_FMRADIO);
