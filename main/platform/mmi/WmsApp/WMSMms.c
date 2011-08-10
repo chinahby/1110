1642b78da533daa428f6094b5de9fc34d9ce1dff
wangliang <wangliang@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-08-10 06:35:19 +0000

    MMS 备份
    git-svn-id: svn://127.0.0.1@4029 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/WmsApp/WMSMms.c
+++ main/platform/mmi/WmsApp/WMSMms.c
@@ -82,12 +82,17 @@ static void MMI_SocketSend(void *pData);
 static void SocketReadableCB(void* pSocketData);
 static void SocketConnect_OnTimeout(void* pData);
 static void ConnectError(void* pDdata, int nError);
+static int MMS_Encode_header(uint8* mms_context,uint8 *phonenum, uint8 *subject);
+static int MMS_WSP_Encode_UINTVAR(uint8* buf, int val);
+static int MMS_Encode_MsgBody(MMS_WSP_ENCODE_SEND* encdata,char* hPDU, int* hLen);
+static int MMS_CreateSmilFile(MMS_MESSAGE_TYPE type,boolean isText);
+static int MMS_GetFileContent(uint8* encbuf,char* FileName,MMS_MESSAGE_TYPE type);
+static int MMS_EncodeText(uint8* encbuf,uint8 *text,int len);
 
-
+//<smil><head><layout><root-layout width="220px" height="96px" background-color="#FFFFFF" /><region id="Image" width="128px" height="96px" top="0px" left="0px" fit="meet" /><region id="Text" width="220px" height="0px" top="96px" left="0px" fit="meet" /></layout></head><body><par dur="5000ms" ><img src="1.jpg" region="Image" /></par></body></smil>
 /*
 ** 彩信里面用到的数据类型
 */
-#if 0
 const char* db_mms2text[76] =
 {
 	"*/*",
@@ -95,93 +100,374 @@ const char* db_mms2text[76] =
     "text/html",
     "text/plain",
     "text/x-hdml",
-    "text/x-ttml"),
-    "text/x-vCalendar"),
-    "text/x-vCard"),
-    "text/vnd.wap.wml"),
-    ("text/vnd.wap.wmlscript"),
-    ("text/vnd.wap.wta-event"),
-    ("multipart/*"),
-    ("multipart/mixed"),
-    ("multipart/form-data"),
-    ("multipart/byterantes"),
-    ("multipart/alternative"),  //0x0f
-    ("application/*"),
-    ("application/java-vm"),
-    ("application/x-www-form-urlencoded"),
-    ("application/x-hdmlc"),
-    ("application/vnd.wap.wmlc"),
-    ("application/vnd.wap.wmlscriptc"),
-    ("application/vnd.wap.wta-eventc"),
-    ("application/vnd.wap.uaprof"),
-    ("application/vnd.wap.wtls-ca-certificate"),
-    ("application/vnd.wap.wtls-user-certificate"),
-    ("application/x-x509-ca-cert"),
-    ("application/x-x509-user-cert"),
-    ("image/*"),
-    ("image/gif"),
-    ("image/jpeg"),
-    ("image/tiff"),   //0x1f
-    ("image/png"),
-    ("image/vnd.wap.wbmp"),
-    ("application/vnd.wap.multipart.*"),
-    ("multipart/mixed"),
-/*    ("application/vnd.wap.multipart.mixed"),*/
-    ("application/vnd.wap.multipart.form-data"),
-    ("application/vnd.wap.multipart.byteranges"),
-/*    ("application/vnd.wap.multipart.alternative"),*/
-    ("multipart/alternative"),
-    ("application/xml"),
-    ("text/xml"),
-    ("application/vnd.wap.wbxml"),
-    ("application/x-x968-cross-cert"),
-    ("application/x-x968-ca-cert"),
-    ("application/x-x968-user-cert"),
-    ("text/vnd.wap.si"),
-    ("application/vnd.wap.sic"),
-    ("text/vnd.wap.sl"),  //0x2f
-    ("application/vnd.wap.slc"),
-    ("text/vnd.wap.co"),
-    ("application/vnd.wap.coc"),
-    ("multipart/related"),
-/*    ("application/vnd.wap.multipart.related"),*/
-    ("application/vnd.wap.sia"),
-    ("text/vnd.wap.connectivity-xml"),
-    ("application/vnd.wap.connectivity-wbxml"),
-    ("application/pkcs7-mime"),
-    ("application/vnd.wap.hashed-certificate"),
-    ("application/vnd.wap.signed-certificate"),
-    ("application/vnd.wap.cert-response"),
-    ("application/xhtml+xml"),
-    ("application/wml+xml"),
-    ("text/css"),
-    ("application/vnd.wap.mms-message"),
-    ("application/vnd.wap.rollover-certificate"),
-    ("application/vnd.wap.locc+wbxml"),
-    ("application/vnd.wap.loc+xml"),
-    ("application/vnd.syncml.dm+wbxml"),
-    ("application/vnd.syncml.dm+xml"),
-    ("application/vnd.syncml.notification"),
-    ("application/vnd.wap.xhtml+xml"),
-    ("application/vnd.wv.csp.cir"),
-    ("application/vnd.oma.dd+xml"),
-    ("application/vnd.oma.drm.message"),
-    ("application/vnd.oma.drm.content"),
-    ("application/vnd.oma.drm.rights+xml"),
-    ("application/vnd.oma.drm.rights+wbxml"),
+    "text/x-ttml",
+    "text/x-vCalendar",
+    "text/x-vCard",
+    "text/vnd.wap.wml",
+    "text/vnd.wap.wmlscript",
+    "text/vnd.wap.wta-event",
+    "multipart/*",
+    "multipart/mixed",
+    "multipart/form-data",
+    "multipart/byterantes",
+    "multipart/alternative",  //0x0f
+    "application/*",
+    "application/java-vm",
+    "application/x-www-form-urlencoded",
+    "application/x-hdmlc",
+    "application/vnd.wap.wmlc",
+    "application/vnd.wap.wmlscriptc",
+    "application/vnd.wap.wta-eventc",
+    "application/vnd.wap.uaprof",
+    "application/vnd.wap.wtls-ca-certificate",
+    "application/vnd.wap.wtls-user-certificate",
+    "application/x-x509-ca-cert",
+    "application/x-x509-user-cert",
+    "image/*",
+    "image/gif",
+    "image/jpeg",
+    "image/tiff",   //0x1f
+    "image/png",
+    "image/vnd.wap.wbmp",
+    "application/vnd.wap.multipart.*",
+    "multipart/mixed",
+/*    "application/vnd.wap.multipart.mixed",*/
+    "application/vnd.wap.multipart.form-data",
+    "application/vnd.wap.multipart.byteranges",
+/*    "application/vnd.wap.multipart.alternative",*/
+    "multipart/alternative",
+    "application/xml",
+    "text/xml",
+    "application/vnd.wap.wbxml",
+    "application/x-x968-cross-cert",
+    "application/x-x968-ca-cert",
+    "application/x-x968-user-cert",
+    "text/vnd.wap.si",
+    "application/vnd.wap.sic",
+    "text/vnd.wap.sl",  //0x2f
+    "application/vnd.wap.slc",
+    "text/vnd.wap.co",
+    "application/vnd.wap.coc",
+    "multipart/related",
+/*    "application/vnd.wap.multipart.related",*/
+    "application/vnd.wap.sia",
+    "text/vnd.wap.connectivity-xml",
+    "application/vnd.wap.connectivity-wbxml",
+    "application/pkcs7-mime",
+    "application/vnd.wap.hashed-certificate",
+    "application/vnd.wap.signed-certificate",
+    "application/vnd.wap.cert-response",
+    "application/xhtml+xml",
+    "application/wml+xml",
+    "text/css",
+    "application/vnd.wap.mms-message",
+    "application/vnd.wap.rollover-certificate",
+    "application/vnd.wap.locc+wbxml",
+    "application/vnd.wap.loc+xml",
+    "application/vnd.syncml.dm+wbxml",
+    "application/vnd.syncml.dm+xml",
+    "application/vnd.syncml.notification",
+    "application/vnd.wap.xhtml+xml",
+    "application/vnd.wv.csp.cir",
+    "application/vnd.oma.dd+xml",
+    "application/vnd.oma.drm.message",
+    "application/vnd.oma.drm.content",
+    "application/vnd.oma.drm.rights+xml",
+    "application/vnd.oma.drm.rights+wbxml",
 };
-#endif
 
-int MMS_PDU_Encode(MMS_WSP_DEC_DATA* decdata, char* hPDU, int* hLen, uint8 ePDUType)
+static int MMS_WSP_Encode_UINTVAR(uint8* buf, int val)
 {
+	int i;
+
+	for(i=0;i<5;i++)
+	{
+		buf[4-i] = (uint8)(val & 0x7f);
+		val = val >> 7;
+		if (i)
+			buf[4-i] |= 0x80;
+		if (!val)
+		{
+			return i+1;
+		}
+	}
+	return i;
+}
+
+static int MMS_Encode_header(uint8* mms_context,uint8 *phonenum, uint8 *subject)
+{
+	int i=0;
+    JulianType juDateTime	= {0};
+	char szDataTime[30] = {0};
+    int addresslen = 0;
+    int len = 0;
+    int uintvar_len = 0;
+    char *addresstype = "/TYPE=PLMN";
+    uint8 uintvar[5];
+    
+    uint8* pCurPos = (uint8*)mms_context;	
+	
+
+	//X-Mms-Message-Type(0x8c): m-send-req(0x80)
+	*pCurPos = 0x8c; pCurPos++;
+	*pCurPos = 0x80; pCurPos++;
+
+	//ID
+	GetJulianDate(GETTIMESECONDS(), &juDateTime);
+	SNPRINTF(szDataTime, sizeof(szDataTime) - 1, "%2.2d:%2.2d %2.2d/%2.2d/%4.4d", juDateTime.wHour, juDateTime.wMinute, juDateTime.wDay, juDateTime.wMonth, juDateTime.wYear);
+	*pCurPos = 0x98; pCurPos++;
+	STRNCPY((char*)pCurPos,szDataTime,STRLEN(szDataTime));
+	pCurPos += STRLEN(szDataTime);
+	*pCurPos = 0x00; pCurPos++;
+	
+	//X-Mms-MMS-Version(0x8d):v1.0(0x90)
+	*pCurPos = 0x8d; pCurPos++;
+	*pCurPos = 0x90; pCurPos++;	
+	
+	//from
+	*pCurPos = 0x89; pCurPos++;
+	*pCurPos = 0x01; pCurPos++;
+	*pCurPos = 0x81; pCurPos++;                         
+
+	/*
+	** eg: 13332941841 + addresstype
+	*/
+	//To(0x97)
+	addresslen = STRLEN((char*)phonenum) + STRLEN(addresstype);
+	*pCurPos = 0x97; pCurPos++;
+	*pCurPos = addresslen; pCurPos++;
+	STRNCPY((char*)pCurPos,(char*)phonenum,STRLEN((char*)phonenum));
+	pCurPos += STRLEN((char*)phonenum);
+	STRNCPY((char*)pCurPos,addresstype,STRLEN(addresstype));
+	pCurPos += STRLEN(addresstype);
+	*pCurPos = 0x00; pCurPos++;
+
+	//subject
+	len = STRLEN((char*)subject);
+	if ( len > 0)
+	{
+		*pCurPos = 0x96; pCurPos++;/*Subject*/
+		if (len+1+1 > 30)
+		{
+			*pCurPos = 0x1F; pCurPos++;/*Charset - UTF-8*//*Charset - UTF-8*/
+			uintvar_len = MMS_WSP_Encode_UINTVAR(uintvar,len+1+1);/* +1 = charset value , +1 - null termination*/
+			STRNCPY((char*)pCurPos,(char*)&uintvar[5-uintvar_len],uintvar_len);
+			pCurPos += uintvar_len;
+		}
+		else
+		{
+			uint8 l;
+			l = len+1+1;
+			*pCurPos = l; pCurPos++;
+		}
+
+		*pCurPos = 0x0b; pCurPos++;/*Charset - UTF-8*/
+		STRNCPY((char*)pCurPos,(char*)subject,STRLEN((char*)subject));
+		pCurPos += STRLEN((char*)subject);
+		*pCurPos = 0x00; pCurPos++;
+	}
+	
+	//后面为数据部分
+	return (int)(pCurPos - (uint8*)mms_context);
+}
+
+static int MMS_Encode_MsgBody(MMS_WSP_ENCODE_SEND* encdata,char* hPDU, int* hLen)
+{
+	int head_length = 0;
+	int file_length = 0;
+	int sub_head_length = 0;
+	int content_size = 0;
+	
+	uint8* pCurPos = (uint8*)hPDU;
+
+	//Msg body
+	*pCurPos = 0x84; pCurPos++;
+	*pCurPos = 0x1c; pCurPos++;  //head length
+	*pCurPos = 0xb3; pCurPos++;  //application/vnd.wap.multipart.related类型，对应于附录A里面的0x33+0x80；
+
+	*pCurPos = 0x89; pCurPos++;  //Type参数，见WSP Table38
+	//application/smil
+	*pCurPos = 0x61; pCurPos++;
+	*pCurPos = 0x70; pCurPos++;
+	*pCurPos = 0x70; pCurPos++;
+	*pCurPos = 0x6c; pCurPos++;
+	*pCurPos = 0x69; pCurPos++;
+	*pCurPos = 0x63; pCurPos++;
+	*pCurPos = 0x61; pCurPos++;
+	*pCurPos = 0x74; pCurPos++;
+	*pCurPos = 0x69; pCurPos++;
+	*pCurPos = 0x6F; pCurPos++;
+	*pCurPos = 0x6E; pCurPos++;
+	*pCurPos = 0x2F; pCurPos++;
+	*pCurPos = 0x73; pCurPos++;
+	*pCurPos = 0x6D; pCurPos++;
+	*pCurPos = 0x69; pCurPos++;
+	*pCurPos = 0x6C; pCurPos++;
+	*pCurPos = 0x00; pCurPos++;
+
+	*pCurPos = 0x8A; pCurPos++;  //Start参数,见WSP协议Table 38. Well-Known Parameter Assignments,编码值加0x80
+	//<start>
+	*pCurPos = 0x3C; pCurPos++;
+	*pCurPos = 0x73; pCurPos++;
+	*pCurPos = 0x74; pCurPos++;
+	*pCurPos = 0x61; pCurPos++;
+	*pCurPos = 0x72; pCurPos++;
+	*pCurPos = 0x74; pCurPos++;
+	*pCurPos = 0x3E; pCurPos++;
+	*pCurPos = 0x00; pCurPos++;
+
+	// -------------- files -------------- //
+	//file count, smil file is one
+	if ( encdata->bIsText )
+	{
+		*pCurPos = 0x03; pCurPos++;	//part number
+	}
+	else
+	{
+		*pCurPos = 0x02; pCurPos++;	//part number
+	}
+
+	//head length
+	//file length
+	//sub head length
+	//content
+	
+}
+
+static int MMS_EncodeText(uint8* encbuf,uint8 *text,int len)
+{
+	int head_length = 0;
+	int file_length = 0;
+	int sub_head_length = 0;
+	int content_size = 0;
+	int uintvar_len;
+	uint8 m_buf[128] = {0};
+	uint8 uintvar[5];
+	uint8* pCurPos = m_buf;
+	
+	
+	if ( encbuf == NULL || text == NULL )
+	{
+		return -1;
+	}
+
+	//file use "text.txt"
+	content_size = len;
+	file_length = 8;
+	sub_head_length = 8+5;
+	*pCurPos = sub_head_length; pCurPos++;
+
+	//UTF-8
+	*pCurPos = 0x83; pCurPos++;
+	*pCurPos = 0x85; pCurPos++;
+
+	//FILE NAME
+	STRNCPY((char*)pCurPos,"text.txt",8);
+	pCurPos += 8;
+	*pCurPos = 0x00; pCurPos++;
+
+	*pCurPos = 0x81; pCurPos++;
+	*pCurPos = 0xea; pCurPos++;
+
+	//add Content-ID
+	*pCurPos = 0xc0; pCurPos++;
+	*pCurPos = 0x22; pCurPos++;
+	*pCurPos = 0x3c; pCurPos++;
+	STRNCPY((char*)pCurPos,"text.txt",8);
+	pCurPos += 8;
+	*pCurPos = 0x3e; pCurPos++;
+	*pCurPos = 0x00; pCurPos++;
+
+	//add Content-Location
+	*pCurPos = 0x8e; pCurPos++;
+	STRNCPY((char*)pCurPos,"text.txt",8);
+	pCurPos += 8;
+	*pCurPos = 0x00; pCurPos++;
+
+	head_length = (uint8)(pCurPos-m_buf);
+
+	pCurPos = encbuf;
+
+	//head length
+	*pCurPos = head_length; pCurPos++;
+	uintvar_len = MMS_WSP_Encode_UINTVAR(uintvar,content_size);
+
+	//content length
+	STRNCPY((char*)pCurPos,(char*)&uintvar[5-uintvar_len],uintvar_len);
+	pCurPos += uintvar_len;
+
+	//sub head
+	STRNCPY((char*)pCurPos,(char*)m_buf,head_length);
+	pCurPos += head_length;
+
+	//content
+	STRNCPY((char*)pCurPos,(char*)text,len);
+	pCurPos += len;
+
+	return (int)(pCurPos-encbuf);
+}
+
+static int MMS_GetFileContent(uint8* encbuf,char* FileName,MMS_MESSAGE_TYPE type)
+{
+	int head_length = 0;
+	int file_length = 0;
+	int sub_head_length = 0;
+	int content_size = 0;
+
+	if ( type == MMS_MESSAGE_JPG )
+	{
+		
+	}
+
+	
+}
+
+static int MMS_CreateSmilFile(MMS_MESSAGE_TYPE type,boolean isText)
+{
+	
+}
+
+int MMS_PDU_Encode(MMS_WSP_ENCODE_SEND* encdata, uint8* hPDU, int* hLen, uint8 ePDUType)
+{
+	int head_len = 0;
+	IFile* pIFile = NULL;
+    IFileMgr *pIFileMgr = NULL;
+    FileInfo pInfo = {0};
+
+    
+	if ( hPDU == NULL || hLen == NULL || encdata == NULL )
+	{
+		return MMC_NOMEM;
+	}
+
+
+	head_len = MMS_Encode_header(hPDU,encdata->hTo,encdata->hSubject);
+
 	return 0;
+
+	
+}
+
+int MMS_WSP_DecodeMessage(uint8* pData, int iDataLen,  uint8* hContentType, uint8* hBody,int* iBodyLen)
+{
+	int charset, iPart, dec = MMC_GENERIC, ret = MMC_GENERIC, acc, contenttypelen, iDataOffset,consumed;
+	boolean bIsMultipart, completed = FALSE;
+
+	contenttypelen = MMS_WSP_GetValueLen(pData, iDataLen, &iDataOffset);
+
+	if (contenttypelen != MMS_DECODER_ERROR_VALUE)
+	{
+
+	}
 }
 
 int MMS_PDU_Decode(MMS_WSP_DEC_DATA* decdata,uint8* ptr, int datalen,uint8 *ePDUType)
 {
 	int len,i=0,iDataOffset,j;
 	uint8* pchar;
+	int ret = MMC_OK;
 
+	
 	if ( decdata == NULL)
 	{
 		return MMC_GENERIC;
@@ -219,6 +505,23 @@ int MMS_PDU_Decode(MMS_WSP_DEC_DATA* decdata,uint8* ptr, int datalen,uint8 *ePDU
 			
 		case 0x84:
 			*ePDUType = MMS_PDU_RETRIEVE_CONF;
+			*ePDUType = MMS_PDU_RETRIEVE_CONF;
+			decdata->message.bDelRep = FALSE;
+			decdata->message.bReadRep = FALSE;
+			MEMSET((void*)decdata->message.hCc,0,sizeof(decdata->message.hCc));
+			MEMSET((void*)decdata->message.hContentType,0,sizeof(decdata->message.hContentType));
+			MEMSET((void*)decdata->message.hFrom,0,sizeof(decdata->message.hFrom));
+			MEMSET((void*)decdata->message.hMessageID,0,sizeof(decdata->message.hMessageID));
+			MEMSET((void*)decdata->message.hSubject,0,sizeof(decdata->message.hSubject));
+			MEMSET((void*)decdata->message.hTransactionID,0,sizeof(decdata->message.hTransactionID));
+			MEMSET((void*)decdata->message.hTo,0,sizeof(decdata->message.hTo));
+			decdata->message.hBody = NULL;
+			decdata->message.iBodyLen = 0;
+			decdata->message.iRetrieveStatus = 0x80;
+			decdata->message.iDate = 0;
+			decdata->message.iPriority       = MMS_PRIORITY_NORMAL;
+			decdata->message.iMessageClass   = MMS_CLASS_PERSONAL;
+			decdata->message.iDeliveryReport = MMS_DELREP_STATUS_FLAG_NOREQ;
 			break;
 			
 		case 0x86:
@@ -272,7 +575,27 @@ int MMS_PDU_Decode(MMS_WSP_DEC_DATA* decdata,uint8* ptr, int datalen,uint8 *ePDU
 			case 0x84:/* content-type */
 			{
 				i++;
-				return MMC_OK;
+				if (*ePDUType == MMS_PDU_RETRIEVE_CONF)
+				{
+					if (decdata->message.iRetrieveStatus == 0x80)
+					{
+						int decret;
+						decret = MMS_WSP_DecodeMessage(&ptr[i], datalen-i,
+							decdata->message.hContentType,
+							decdata->message.hBody, &decdata->message.iBodyLen);
+						if (decret == MMC_OK)
+							return ret;
+						else
+							return decret;
+					}
+					else
+						return ret;
+				}
+				else
+				{
+					return MMC_OK;
+				}
+
 				break;
 			}
 
--- main/platform/mmi/WmsApp/WMSMms.h
+++ main/platform/mmi/WmsApp/WMSMms.h
@@ -50,7 +50,8 @@
 #define	MMS_MAX_ADDRESS_COUNT	70
 #define MMS_MAX_SINGLE_ADDRESS_SIZE 90
 #define MMS_MAX_CONTENT_LOCATION	100
-
+#define MMS_MAX_FILE_NAME_PATH      256
+#define MMS_MAX_TEXT_SIZE 256
 #define	MMSCORE_MAX_SERVER_URL_LENGTH	256
 #define MMSCORE_MAX_PROXY_HOST_LENGTH	64
 #define MMSCORE_MAX_PROXY_PORT_LENGTH	4
@@ -61,6 +62,51 @@
 
 #define MMS_DEFAULT_PROXYHOST		("10.0.0.200")
 #define MMS_DEFAULT_PROXYPORT		(80)
+
+/* constant for http status code */
+typedef enum HTTP_RET_CODE{
+	HTTP_CODE_Continue							= 100,
+	HTTP_CODE_Switching_Protocols				= 101,
+	HTTP_CODE_OK								= 200,
+	HTTP_CODE_Created							= 201,
+	HTTP_CODE_Accepted							= 202,
+	HTTP_CODE_Non_Authoritative_Information		= 203,
+	HTTP_CODE_No_Content						= 204,
+	HTTP_CODE_Reset_Content						= 205,
+	HTTP_CODE_Partial_Content					= 206,
+	HTTP_CODE_Multiple_Choices					= 300,
+	HTTP_CODE_Moved_Permanently					= 301,
+	HTTP_CODE_Found								= 302,
+	HTTP_CODE_See_Other							= 303,
+	HTTP_CODE_Not_Modified						= 304,
+	HTTP_CODE_Use_Proxy							= 305,
+	HTTP_CODE_Temporary_Redirect				= 307,
+	HTTP_CODE_Bad_Request						= 400,
+	HTTP_CODE_Unauthorized						= 401,
+	HTTP_CODE_Payment_Required					= 402,
+	HTTP_CODE_Forbidden							= 403,
+	HTTP_CODE_Not_Found							= 404,
+	HTTP_CODE_Method_Not_Allowed				= 405,
+	HTTP_CODE_Not_Acceptable					= 406,
+	HTTP_CODE_Proxy_Authentication_Required		= 407,
+	HTTP_CODE_Request_Time_out					= 408,
+	HTTP_CODE_Conflict							= 409,
+	HTTP_CODE_Gone								= 410,
+	HTTP_CODE_Length_Required					= 411,
+	HTTP_CODE_Precondition_Failed				= 412,
+	HTTP_CODE_Request_Entity_Too_Large			= 413,
+	HTTP_CODE_Request_URI_Too_Large				= 414,
+	HTTP_CODE_Unsupported_Media_Type			= 415,
+	HTTP_CODE_Requested_range_not_satisfiable	= 416,
+	HTTP_CODE_Expectation_Failed				= 417,
+	HTTP_CODE_Internal_Server_Error				= 500,
+	HTTP_CODE_Not_Implemented					= 501,
+	HTTP_CODE_Bad_Gateway						= 502,
+	HTTP_CODE_Service_Unavailable				= 503,
+	HTTP_CODE_Gateway_Time_out					= 504,
+	HTTP_CODE_HTTP_Version_not_supported		= 505
+}HTTP_RET_CODE;
+
 /*
 ** 彩信请求和通知类型
 */
@@ -137,6 +183,15 @@ typedef enum _setting_validity_
 	PREF_VALIDITY_MAXIMUM = 999
 }MMS_VALIDITY;
 
+typedef enum _mms_message_type_
+{
+	MMS_MESSAGE_PNG,
+	MMS_MESSAGE_JPG,
+	MMS_MESSAGE_GIF,
+	MMS_MESSAGE_BMP,
+	MMS_MESSAGE_VIDEO,
+	MMS_MESSAGE_SOUND,
+}MMS_MESSAGE_TYPE;
 /*
 ** 收到的彩信内容
 */
@@ -200,6 +255,17 @@ typedef struct _wsp_decoder_data_delrep_received
 	int	iStatus;
 	int	iDate;
 }MMS_WSP_DEC_DELREP_RECEIVED;
+
+typedef struct _wsp_encoder_data_sent
+{
+	uint8 hTo[MMS_MAX_SINGLE_ADDRESS_SIZE];
+	uint8 hSubject[MMS_MAX_SUBJECT_SIZE];
+	uint8 hCc[MMS_MAX_SINGLE_ADDRESS_SIZE];
+	uint8 FilePath[MMS_MAX_FILE_NAME_PATH];
+	uint8 Text[MMS_MAX_TEXT_SIZE];
+	MMS_MESSAGE_TYPE  bMsgType;
+	boolean bIsText;
+}MMS_WSP_ENCODE_SEND;
 
 typedef union _wsp_decoder_data_
 {
@@ -275,9 +341,9 @@ int MMS_PDU_PutPriority(int in_priority, int* out_priority);
 int MMS_PDU_PutDeliveryReportStatus(int in_Status, int* out_Status);
 
 
-int MMS_PDU_Encode(MMS_WSP_DEC_DATA* decdata,char* hPDU, int* hLen, uint8 ePDUType);
+int MMS_PDU_Encode(MMS_WSP_ENCODE_SEND* encdata,uint8* hPDU, int* hLen, uint8 ePDUType);
 int MMS_PDU_Decode(MMS_WSP_DEC_DATA* decdata,uint8* ptr, int datalen,uint8* ePDUType);
-
+int MMS_WSP_DecodeMessage(uint8* pData, int iDataLen,  uint8* hContentType, uint8* hBody,int* iBodyLen);
 
 boolean MMS_GetProxySettings(boolean *bUseProxy,char* hProxyHost, int* iProxyPort);
 boolean MMS_SetProxySettings(boolean bUseProxy,char* hProxyHost, int iProxyPort);
