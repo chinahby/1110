2425b420f24913f5a9498fd385da6141ccb05204
pangji <pangji@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-11-07 09:07:14 +0000

    git-svn-id: svn://127.0.0.1@4253 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/build/ms/custc180.h
+++ main/build/ms/custc180.h
@@ -10,6 +10,7 @@ Copyright (c) 2001-2010 by QUALCOMM Incorporated.  All Rights Reserved.
 #endif
 
 #ifdef CUST_EDITION
+#define FEATURE_VERSION_C180
 #define FEATURE_OEMOMH
 //Add by zzg 2010_10_13
 #define FEATURE_TORCH_KEY_CAMERA			//手电筒功能按键
