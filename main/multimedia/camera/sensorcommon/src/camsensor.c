d633d39e83bc42ff038356174d3185699eb1cee0
lixiaolong <lixiaolong@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-11-24 01:44:58 +0000

    覆盖原先的双摄像头驱动 该文件前俩次的提交有点问题
    git-svn-id: svn://127.0.0.1@4367 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/multimedia/camera/sensorcommon/src/camsensor.c
+++ main/multimedia/camera/sensorcommon/src/camsensor.c
@@ -397,22 +397,6 @@
 #include "camsensor_sic110a.h"
 #endif
 
-#ifdef USE_CAMSENSOR_SID130B
-#include "camsensor_sid130b.h"
-#endif
-
-#ifdef USE_CAMSENSOR_MICRON_SIV121D_0M3
-#include "camsensor_siv121d.h"
-#endif
-
-#ifdef USE_CAMSENSOR_SP0838
-#include "camsensor_sp0838.h"
-#endif
-
-#ifdef USE_CAMSENSOR_SP0A18
-#include "camsensor_sp0a18.h"
-#endif
-
 #ifdef USE_CAMSENSOR_OV7675
 #include "camsensor_ov7675.h"
 #endif
@@ -796,21 +780,6 @@ LOCAL boolean (*camsensor_detect_table[])(camsensor_function_table_type *, camct
   camsensor_sic110a_init,
 #endif
 
-#ifdef USE_CAMSENSOR_SID130B
-  camsensor_SID130B_init,
-#endif
-
-#ifdef USE_CAMSENSOR_MICRON_SIV121D_0M3
-  camsensor_siv121d_0m3_init,
-#endif
-
-#ifdef USE_CAMSENSOR_SP0838
-  camsensor_sp0838_init,
-#endif
-
-#ifdef USE_CAMSENSOR_SP0A18
-  camsensor_sp0a18_init,
-#endif
 
 #ifdef USE_CAMSENSOR_OV7675
   camsensor_ov7675_init
@@ -2321,31 +2290,8 @@ else {
     pm_vote_vreg_switch( PM_ON_CMD, PM_VREG_MMC_ID, PM_VOTE_VREG_MMC_APP__MMC );
 }
 #endif /* CAMERA_USE_PMIC_TO_POWER_SENSOR */
-
-#ifdef FEATURE_CAMERA_MULTI_SENSOR
-	MSG_FATAL("camsensor_power_on camera_asi = %d",camera_asi,0,0);
-	camera_timed_wait(13);
-	CAMERA_CONFIG_GPIO(CAMSENSOR1_POWER_PIN);
-	CAMERA_CONFIG_GPIO(CAMSENSOR2_POWER_PIN);
-	
-	camera_timed_wait(10);
-	if ( camera_asi == 0)
-	{
-		gpio_out(CAMSENSOR1_POWER_PIN, (GPIO_ValueType)0);
-  		gpio_out(CAMSENSOR2_POWER_PIN, (GPIO_ValueType)0);
-	}
-	else if ( camera_asi == 1)
-	{
-		gpio_out(CAMSENSOR1_POWER_PIN, (GPIO_ValueType)1);
-  		gpio_out(CAMSENSOR2_POWER_PIN, (GPIO_ValueType)1);
-	}
-	else
-	{
-		gpio_out(CAMSENSOR1_POWER_PIN, (GPIO_ValueType)1);
-  		gpio_out(CAMSENSOR2_POWER_PIN, (GPIO_ValueType)0);
-	}
-#else
-#if 0
+   
+  /* Enable the Sensor Module */
   #ifdef GPIO_CAMIF_EN_ON_V
   CAMERA_CONFIG_GPIO(CAMIF_EN_N);
   #if defined(FEATURE_CAMERA_SP0828)
@@ -2354,15 +2300,6 @@ else {
   gpio_out(CAMIF_EN_N, (GPIO_ValueType)GPIO_CAMIF_EN_ON_V);
   #endif
   #endif
-#endif
-	CAMERA_CONFIG_GPIO(CAMSENSOR1_POWER_PIN);
-	CAMERA_CONFIG_GPIO(CAMSENSOR2_POWER_PIN);
-	
-	camera_timed_wait(10);
-	gpio_out(CAMSENSOR1_POWER_PIN, (GPIO_ValueType)0);
-  	gpio_out(CAMSENSOR2_POWER_PIN, (GPIO_ValueType)1);
-#endif/* FEATURE_CAMERA_MULTI_SENSOR */
-
   (void) camsensor_config_camclk_po(camsensor_camclk_po_hz);
   camera_timed_wait(13);
 #ifdef FEATURE_CAMERA_MOBICAT_CAMERA_CONFIG_H
@@ -2470,22 +2407,14 @@ else {
 #endif /* CAMERA_USE_PMIC_TO_POWER_SENSOR */
   
 /* Disable the Sensor when not in use */
-#ifdef FEATURE_CAMERA_MULTI_SENSOR
-	gpio_out(CAMSENSOR1_POWER_PIN, (GPIO_ValueType)1);
-	gpio_out(CAMSENSOR2_POWER_PIN, (GPIO_ValueType)0);  
-#else
-	#if 0
-	#ifdef GPIO_CAMIF_EN_OFF_V
-		#if defined(FEATURE_CAMERA_SP0828)
-			gpio_out(CAMIF_EN_N, (GPIO_ValueType)GPIO_CAMIF_EN_ON_V);
-		#else
-			gpio_out(CAMIF_EN_N, (GPIO_ValueType)GPIO_CAMIF_EN_OFF_V);
-		#endif
-	#endif
-	#endif
-	gpio_out(CAMSENSOR1_POWER_PIN, (GPIO_ValueType)1);
-  	gpio_out(CAMSENSOR2_POWER_PIN, (GPIO_ValueType)1);
-#endif
+
+  #ifdef GPIO_CAMIF_EN_OFF_V
+  #if defined(FEATURE_CAMERA_SP0828)
+  gpio_out(CAMIF_EN_N, (GPIO_ValueType)GPIO_CAMIF_EN_ON_V);
+  #else
+  gpio_out(CAMIF_EN_N, (GPIO_ValueType)GPIO_CAMIF_EN_OFF_V);
+  #endif
+  #endif
   /* If the sensor need to be initialized next time when powered
    * on, then you need to clear camsensor_initialized */
   camsensor_initialized = FALSE;
@@ -4319,62 +4248,47 @@ SIDE EFFECTS
 ===========================================================================*/
 boolean camsensor_init (void)
 {
-	if (camsensor_initialized)
-	{
-		return TRUE;
-	}
+  if (camsensor_initialized)
+  {
+    return TRUE;
+  }
 
 #ifndef FEATURE_CAMERA_MULTI_SENSOR
-  	camsensor_id = CAMSENSOR_ID_MAX;
+  camsensor_id = CAMSENSOR_ID_MAX;
 #endif /* FEATURE_CAMERA_MULTI_SENSOR */
 
 #ifdef FEATURE_CAMERA_SENSOR_AUTO_DETECT
-#ifdef FEATURE_CAMERA_MULTI_SENSOR
-	#ifdef FEATURE_NI_GPIO
-  		for (camsensor_id = CAMSENSOR_NI_GPIO; camsensor_id < CAMSENSOR_ID_MAX; ++camsensor_id)
-	#else /* FEATURE_NI_GPIO */
-		/* camsensor_id starts with -1 instead of CAMSENSOR_OV2620 as before since
-		 * customer may not want to include USE_CAMSENSOR_OV2620 in the build in which
-		 * case this shoots an error. Care should be taken to see that camsensor_id starts
-		 * with the first value of the enum "camsensor_sensor_model_type" */
-  		for (camsensor_id = (camsensor_sensor_model_type) 0; camsensor_id < CAMSENSOR_ID_MAX; ++camsensor_id)
-	#endif /* FEATURE_NI_GPIO */
-#else
-	#ifdef FEATURE_NI_GPIO
-  		for (camsensor_id = CAMSENSOR_NI_GPIO; camsensor_id < CAMSENSOR_ID_MAX; ++camsensor_id)
-	#else /* FEATURE_NI_GPIO */
-		/* camsensor_id starts with -1 instead of CAMSENSOR_OV2620 as before since
-		 * customer may not want to include USE_CAMSENSOR_OV2620 in the build in which
-		 * case this shoots an error. Care should be taken to see that camsensor_id starts
-		 * with the first value of the enum "camsensor_sensor_model_type" */
-  		for (camsensor_id = (camsensor_sensor_model_type) 0; camsensor_id < CAMSENSOR_ID_MAX; ++camsensor_id)
-	#endif /* FEATURE_NI_GPIO */
+#ifndef FEATURE_CAMERA_MULTI_SENSOR
+#ifdef FEATURE_NI_GPIO
+  for (camsensor_id = CAMSENSOR_NI_GPIO; camsensor_id < CAMSENSOR_ID_MAX; ++camsensor_id)
+#else /* FEATURE_NI_GPIO */
+/* camsensor_id starts with -1 instead of CAMSENSOR_OV2620 as before since
+ * customer may not want to include USE_CAMSENSOR_OV2620 in the build in which
+ * case this shoots an error. Care should be taken to see that camsensor_id starts
+ * with the first value of the enum "camsensor_sensor_model_type" */
+  for (camsensor_id = (camsensor_sensor_model_type) 0; camsensor_id < CAMSENSOR_ID_MAX; ++camsensor_id)
+#endif /* FEATURE_NI_GPIO */
 #endif /* FEATURE_CAMERA_MULTI_SENSOR */
-	{
-		MSG_FATAL("camsensor_id================%d",camsensor_id,0,0);
-		if (camsensor_detect_table[camsensor_id])
-		{
-		  camctrl_init_tbl();
-		  camsensor_init_func_tbl();
-		  camsensor_initialized = (*camsensor_detect_table[camsensor_id])(&camsensor_function_table,
-		                                                                  &camctrl_tbl);
-		}
+  {
+  	MSG_FATAL("camsensor_id================%d",camsensor_id,0,0);
+    if (camsensor_detect_table[camsensor_id])
+    {
+      camctrl_init_tbl();
+      camsensor_init_func_tbl();
+      camsensor_initialized = (*camsensor_detect_table[camsensor_id])(&camsensor_function_table,
+                                                                      &camctrl_tbl);
+    }
 
-#ifdef FEATURE_CAMERA_MULTI_SENSOR
-	    if (camsensor_initialized)
-	    {
-	      	break;
-	    }
-#else
-		if (camsensor_initialized)
-	    {
-	      	break;
-	    }
+#ifndef FEATURE_CAMERA_MULTI_SENSOR
+    if (camsensor_initialized)
+    {
+      break;
+    }
 #endif /* nFEATURE_CAMERA_MULTI_SENSOR */
-	}
+  }
 #endif /* FEATURE_CAMERA_SENSOR_AUTO_DETECT */
 
-	return camsensor_initialized;
+  return camsensor_initialized;
 } /* camsensor_init */
 
 /*===========================================================================
