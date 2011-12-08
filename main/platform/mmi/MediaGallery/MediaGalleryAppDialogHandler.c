cb44096ff31d419da46c00fb4a0e6a71dcdf9464
xuhui <xuhui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-12-08 06:13:59 +0000

    git-svn-id: svn://127.0.0.1@4484 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/MediaGallery/MediaGalleryAppDialogHandler.c
+++ main/platform/mmi/MediaGallery/MediaGalleryAppDialogHandler.c
@@ -5398,6 +5398,33 @@ static boolean MGAppPopupMenu_OnDetail(CMediaGalleryApp* pMe,
    case EVT_COMMAND:
       return TRUE;
 
+#ifdef FEATURE_LCD_TOUCH_ENABLE//wlh add for LCD touch   
+   case EVT_PEN_UP:
+       {
+           AEEDeviceInfo devinfo;
+           int nBarH ;
+           AEERect rc;
+           int16 wXPos = (int16)AEE_GET_X(dwParam);
+           int16 wYPos = (int16)AEE_GET_Y(dwParam);
+           MSG_FATAL("MGAppPopupMenu_OnDetail wXPos=%d ,wYPos=%d",wXPos,wYPos,0);
+           nBarH = GetBottomBarHeight(pMe->m_pDisplay);
+   
+           MEMSET(&devinfo, 0, sizeof(devinfo));
+           ISHELL_GetDeviceInfo(pMe->m_pShell, &devinfo);
+           SETAEERECT(&rc, 0, devinfo.cyScreen-nBarH, devinfo.cxScreen, nBarH);
+
+           if(MEDIAGALLERYAPP_PT_IN_RECT(wXPos,wYPos,rc))
+           {
+               if(wXPos >= rc.x + (rc.dx/3)*2 && wXPos < rc.x + (rc.dx/3)*3 )//×ó
+               {                       
+                    boolean rt = MGAppPopupMenu_OnDetail(pMe,EVT_KEY,AVK_CLR,0);
+                    return rt;
+               }
+           }
+       }
+       break;
+#endif //FEATURE_LCD_TOUCH_ENABLE
+
    default:
       break;
    }
