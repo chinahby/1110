93fbd7c4f1f7aa92eae0ef7ed8555466a64aabf2
pengyuangui <pengyuangui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-12-13 06:46:42 +0000

    git-svn-id: svn://127.0.0.1@4567 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/WmsApp/WMSDialogHandler.c
+++ main/platform/mmi/WmsApp/WMSDialogHandler.c
@@ -1798,6 +1798,43 @@ static boolean  IDD_PWD_Handler(void       *pUser,
                 }
             }
             return TRUE;
+#ifdef FEATURE_LCD_TOUCH_ENABLE
+        case EVT_PEN_UP:
+            {
+                AEEDeviceInfo devinfo;
+                int nBarH ;
+                AEERect rc;
+                int16 wXPos = (int16)AEE_GET_X(dwParam);
+                int16 wYPos = (int16)AEE_GET_Y(dwParam);
+
+                nBarH = GetBottomBarHeight(pMe->m_pDisplay);
+        
+                MEMSET(&devinfo, 0, sizeof(devinfo));
+                ISHELL_GetDeviceInfo(pMe->m_pShell, &devinfo);
+                SETAEERECT(&rc, 0, devinfo.cyScreen-nBarH, devinfo.cxScreen, nBarH);
+
+                if(WMSAPP_PT_IN_RECT(wXPos,wYPos,rc))
+                {
+                    if(wXPos >= rc.x && wXPos < rc.x + (rc.dx/3) )//×ó
+                    {
+                        boolean rt =  ISHELL_PostEvent(pMe->m_pShell,AEECLSID_WMSAPP,EVT_USER,AVK_SELECT,0);
+                        return rt;
+                    }
+                    else if(wXPos >= rc.x + (rc.dx/3)   && wXPos < rc.x + (rc.dx/3)*2 )//×ó
+                    {
+                         boolean rt = ISHELL_PostEvent(pMe->m_pShell,AEECLSID_WMSAPP,EVT_USER,AVK_INFO,0);
+                         return rt;
+                    }
+                    else if(wXPos >= rc.x + (rc.dx/3)*2 && wXPos < rc.x + (rc.dx/3)*3 )//×ó
+                    {                       
+                         boolean rt = ISHELL_PostEvent(pMe->m_pShell,AEECLSID_WMSAPP,EVT_USER,AVK_CLR,0);
+                         return rt;
+                    }
+                }
+
+            }
+                                    break;
+#endif   
             
         default:
             break;
