7bc98e8ea15dad5b37174ae9f3a6e23138e876f8
miaoxiaoming <miaoxiaoming@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2011-12-28 02:22:39 +0000

    7775 添加MDP的驱动适配
    git-svn-id: svn://127.0.0.1@4821 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/multimedia/display/lcd/src/disp_ic_tm7775_tft20.c
+++ main/multimedia/display/lcd/src/disp_ic_tm7775_tft20.c
@@ -12,9 +12,14 @@
 
 //For lcd QVGA TM7775R driver
 #define DISP_LCD_18BPP(x)                 ((uint16)(x))
-#define DISP_LCD_HORZ_RAM_ADDR_POS_1_ADDR 0x2A // Register to set col start
-#define DISP_LCD_VERT_RAM_ADDR_POS_1_ADDR 0x2B // Register to set row start
-#define DISP_LCD_CMD_RAMWR                0x2C // RAM Data Write
+#define DISP_LCD_HORZ_RAM_ADDR_POS_1_ADDR 0x38 // Register to set col start
+#define DISP_LCD_HORZ_RAM_ADDR_POS_2_ADDR 0x39 // Register to set col end
+#define DISP_LCD_VERT_RAM_ADDR_POS_1_ADDR 0x36 // Register to set row start
+#define DISP_LCD_VERT_RAM_ADDR_POS_2_ADDR 0x37 // Register to set row end
+#define DISP_LCD_RAM_ADDR_SET_1_ADDR      0x21 // Start address - col
+#define DISP_LCD_RAM_ADDR_SET_2_ADDR      0x20 // Start address - row
+#define DISP_LCD_CMD_RAMWR                0x22 // RAM Data Write
+
 
 #ifdef __GNUC__
 static uint32 MDP_DISP_SCR_TM7775R[MDP_LCD_SCR_SIZE][MDP_LCD_SCR_LEN]
@@ -31,19 +36,27 @@ __align(16) static uint32 MDP_DISP_SCR_TM7775R[MDP_LCD_SCR_SIZE][MDP_LCD_SCR_LEN
     SET_LCD_DATA_ADDR, //4 set LCD data port address
     LCD_DATA_WH,   //5
     
-    SEND_LCD_CNTL(DISP_LCD_18BPP(DISP_LCD_HORZ_RAM_ADDR_POS_1_ADDR)),//6
-    NULL,              //7
-    NULL,              //8
-    NULL,              //9
-    NULL,              //10
-    SEND_LCD_CNTL(DISP_LCD_18BPP(DISP_LCD_VERT_RAM_ADDR_POS_1_ADDR)),//11
-    NULL,              //12
-    NULL,              //13
-    NULL,              //14
-    NULL,              //15
-    SEND_LCD_CNTL(DISP_LCD_18BPP(DISP_LCD_CMD_RAMWR)),//16
-    
-    RETURN            //17
+  SEND_LCD_CNTL(DISP_LCD_18BPP(DISP_LCD_HORZ_RAM_ADDR_POS_1_ADDR)),//6
+  NULL,             //7
+  NULL,             //8
+  SEND_LCD_CNTL(DISP_LCD_18BPP(DISP_LCD_HORZ_RAM_ADDR_POS_2_ADDR)),//9
+  NULL,             //10
+  NULL,             //11
+  SEND_LCD_CNTL(DISP_LCD_18BPP(DISP_LCD_VERT_RAM_ADDR_POS_1_ADDR)),//12
+  NULL,             //13
+  NULL,             //14
+  SEND_LCD_CNTL(DISP_LCD_18BPP(DISP_LCD_VERT_RAM_ADDR_POS_2_ADDR)),//15
+  NULL,             //16
+  NULL,             //17
+  SEND_LCD_CNTL(DISP_LCD_18BPP(DISP_LCD_RAM_ADDR_SET_1_ADDR)),//18
+  NULL,             //19
+  NULL,             //20
+  SEND_LCD_CNTL(DISP_LCD_18BPP(DISP_LCD_RAM_ADDR_SET_2_ADDR)),//21
+  NULL,             //22
+  NULL,             //23
+  SEND_LCD_CNTL(DISP_LCD_18BPP(DISP_LCD_CMD_RAMWR)),//24
+      
+  RETURN            //25
 };
 
 static uint8 disp_ic_mdp_getformat(void)
@@ -62,6 +75,21 @@ static uint16 disp_ic_mdp_getscr(uint32 **ppscr)
 
 static void disp_ic_mdp_scrupdate(uint32 *scr, uint32 start_row, uint32 start_col, uint32 end_row, uint32 end_col)
 {
+    scr[7] = SEND_LCD_DATA(DISP_LCD_18BPP(end_row>>8));
+    scr[8] = SEND_LCD_DATA(DISP_LCD_18BPP(end_row));
+    scr[10] = SEND_LCD_DATA(DISP_LCD_18BPP(start_row>>8));
+    scr[11] = SEND_LCD_DATA(DISP_LCD_18BPP(start_row));
+    scr[13] = SEND_LCD_DATA(DISP_LCD_18BPP(end_col>>8));
+    scr[14] = SEND_LCD_DATA(DISP_LCD_18BPP(end_col));
+    scr[16] = SEND_LCD_DATA(DISP_LCD_18BPP(start_col>>8));
+    scr[17] = SEND_LCD_DATA(DISP_LCD_18BPP(start_col));
+    
+    scr[19] = SEND_LCD_DATA(DISP_LCD_18BPP(start_row>>8));
+    scr[20] = SEND_LCD_DATA(DISP_LCD_18BPP(start_row));
+    scr[22] = SEND_LCD_DATA(DISP_LCD_18BPP(start_col>>8));
+    scr[23] = SEND_LCD_DATA(DISP_LCD_18BPP(start_col));
+
+#if 0
     scr[7] = SEND_LCD_DATA(DISP_LCD_18BPP(start_col>>8));
     scr[8] = SEND_LCD_DATA(DISP_LCD_18BPP(start_col));
     scr[9] = SEND_LCD_DATA(DISP_LCD_18BPP(end_col>>8));
@@ -70,6 +98,7 @@ static void disp_ic_mdp_scrupdate(uint32 *scr, uint32 start_row, uint32 start_co
     scr[13] = SEND_LCD_DATA(DISP_LCD_18BPP(start_row));
     scr[14] = SEND_LCD_DATA(DISP_LCD_18BPP(end_row>>8));
     scr[15] = SEND_LCD_DATA(DISP_LCD_18BPP(end_row));
+#endif
 }
 #endif
 
@@ -148,12 +177,10 @@ static void disp_ic_setwindow(uint32 start_row, uint32 start_col, uint32 end_row
 	LCD_WRITE_CMD(0x0022);
 
 }
-#include "err.h"
 static void disp_ic_sleep(boolean bin)
 {
     if(bin)
-    {
-         	
+    {         	
        LCD_WRITE_OUT(0x0007,0x0000);
        LCD_DELAY(25); 	
        LCD_WRITE_OUT(0x0010,0x0003);
@@ -199,7 +226,6 @@ boolean disp_tm7775r_tft20(disp_drv_ic_type *pdispic)
     
     if(id1==0x77&&id2==0x75)
     {
-        
         DISP_IC_INIT_TBL(pdispic);
 		
         return TRUE;
