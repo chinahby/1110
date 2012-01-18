49c5d01aaf690c2b480bd6a4bf3390b0c637afbf
pangji <pangji@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2012-01-18 06:31:27 +0000

    C01相关bug修改
    git-svn-id: svn://127.0.0.1@4968 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/asic/apps/StaticExtensions/OEM/Src/OEMAnnunciator.c
+++ main/asic/apps/StaticExtensions/OEM/Src/OEMAnnunciator.c
@@ -2256,7 +2256,7 @@ static int IAnnunciator_Redraw(IAnnunciator *pMe)
                     }					
 #elif defined(FEATURE_DISP_128X160)
 					bgRect.x = 20;
-                    if(titleLen > 84)
+                    if(1)
                     {
                         bgRect.x = 0;
                         bgRect.dx = 128;
