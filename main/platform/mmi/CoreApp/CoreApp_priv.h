2e6600376586a7ddb590228eeed5d49a088422ab
pangji <pangji@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2012-02-05 02:41:53 +0000

    git-svn-id: svn://127.0.0.1@5005 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CoreApp/CoreApp_priv.h
+++ main/platform/mmi/CoreApp/CoreApp_priv.h
@@ -324,10 +324,10 @@
 #define    PWROFF_ANI_FRAME_COUNT                PWRON_ANI_FRAME_COUNT
 #define    PWROFF_ANI_RATE                       PWRON_ANI_RATE
 #elif defined FEATURE_VERSION_S106
-#define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweronoff.gif"
+#define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweron.gif"
 #define    PWRON_ANI_FRAME_COUNT                 (1)
 #define    PWRON_ANI_RATE                        (4000)
-#define    PWROFF_ANI_FILE                       "fs:/image/pwronoffani/poweronoff.gif"
+#define    PWROFF_ANI_FILE                       "fs:/image/pwronoffani/poweroff.gif"
 #define    PWROFF_ANI_FRAME_COUNT                PWRON_ANI_FRAME_COUNT
 #define    PWROFF_ANI_RATE                       PWRON_ANI_RATE
 #elif defined FEAUTRE_VERSION_N450
