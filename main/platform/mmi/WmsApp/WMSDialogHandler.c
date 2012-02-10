5bc3d54db9c7eee9ae93621d658db6ec63b6f6cc
pengyuangui <pengyuangui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2012-02-10 11:03:38 +0000

    git-svn-id: svn://127.0.0.1@5063 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/WmsApp/WMSDialogHandler.c
+++ main/platform/mmi/WmsApp/WMSDialogHandler.c
@@ -9669,7 +9669,7 @@ static boolean IDD_SENDOPTS_Handler(void   *pUser,
                 {
                     AEERect rect;
                     int ry=0;
-                    #ifdef FEATURE_VERSION_C01
+                    #if defined(FEATURE_VERSION_C01) || defined(FEATURE_VERSION_1110W516)
                     if(i<4 && i != 2)
                     #else
                     if(i<4)
