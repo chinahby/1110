a4dc95da0f085945cf864329d34fe29ff72ad105
pengyuangui <pengyuangui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2012-03-07 01:25:35 +0000

    git-svn-id: svn://127.0.0.1@5229 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CoreApp/CoreAppDialogHandler.c
+++ main/platform/mmi/CoreApp/CoreAppDialogHandler.c
@@ -3212,7 +3212,7 @@ static boolean  IDD_IDLE_Handler(void       *pUser,
 			
             if(pMe->m_pIAnn != NULL)
             {
-                #if defined(FEATURE_VERSION_C180) || defined(FEATURE_VERSION_C11)
+                #if defined(FEATURE_VERSION_C180) || defined(FEATURE_VERSION_C11) || defined(FEATURE_VERSION_1110W516)
                 IANNUNCIATOR_SetHasTitleText(pMe->m_pIAnn, FALSE);
                 #else
                 IANNUNCIATOR_SetHasTitleText(pMe->m_pIAnn, TRUE);//返回待机界面时，要把显示titlebar标志还原成TRUE
@@ -3557,7 +3557,7 @@ static boolean  IDD_IDLE_Handler(void       *pUser,
             (void) ISHELL_CancelTimer(pMe->a.m_pIShell,
                                       CoreApp_SearchingTimer,
                                       pMe);
-             #ifdef FEATURE_VERSION_C180
+              #if defined(FEATURE_VERSION_C180) || defined(FEATURE_VERSION_1110W516)
              IANNUNCIATOR_SetHasTitleText(pMe->m_pIAnn, TRUE);
              #endif
 #ifdef FEATURE_VERSION_C11
