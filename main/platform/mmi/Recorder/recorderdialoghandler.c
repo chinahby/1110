980bd18d4478656e1acf352299486bda0d924dd4
pengyuangui <pengyuangui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2012-05-21 07:22:29 +0000

    git-svn-id: svn://127.0.0.1@5346 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/Recorder/recorderdialoghandler.c
+++ main/platform/mmi/Recorder/recorderdialoghandler.c
@@ -1493,7 +1493,7 @@ static boolean  dialog_handler_of_state_record( Recorder* pme, AEEEvent evt, uin
 					db_items_value_type dbItemValue      = {0};
 					db_get( DB_IN_USE, &dbItemValue);
 
-					//recorder_stop_if( &pme->m_Media);		//Add By zzg 2012_03_01
+					recorder_stop_if( &pme->m_Media);		//Add By zzg 2012_03_01
 					if( dbItemValue.in_use)
 					{
 						MOVE_TO_STATE( STATE_RECORD_LIST);
