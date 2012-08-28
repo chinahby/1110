4addb77c86ebd75e089c99d8444b631b05447293
jiaoyuhai <jiaoyuhai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2012-08-28 09:52:16 +0000

    git-svn-id: svn://127.0.0.1@5488 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/multimedia/camera/sensorcommon/src/camsensor.c
+++ main/multimedia/camera/sensorcommon/src/camsensor.c
@@ -739,7 +739,7 @@ LOCAL camsensor_unactive_fn_type camsensor_unactive_value_table[CAMSENSOR_ID_MAX
 #endif
 
 #ifdef USE_CAMSENSOR_GC0311
-	camsensor_gc0311_ycbcr_unactive,
+	camsensor_GC0311_ycbcr_unactive,
 #endif
 
 #ifdef USE_CAMSENSOR_SP0828
@@ -891,7 +891,7 @@ LOCAL camsensor_active_fn_type camsensor_active_value_table[CAMSENSOR_ID_MAX] =
 #endif
 
 #ifdef USE_CAMSENSOR_GC0311
-	camsensor_gc0311_ycbcr_active,
+	camsensor_GC0311_ycbcr_active,
 #endif
 
 #ifdef USE_CAMSENSOR_SP0828
@@ -1062,7 +1062,7 @@ LOCAL boolean (*camsensor_detect_table[])(camsensor_function_table_type *, camct
 #endif
 
 #ifdef USE_CAMSENSOR_GC0311
-	camsensor_gc0311_init,
+	camsensor_GC0311_init,
 #endif
 
 #ifdef USE_CAMSENSOR_SP0828
--- main/multimedia/camera/sensordriver/camsensor_siv121a/inc/camsensor_gc0311.h
+++ main/multimedia/camera/sensordriver/camsensor_siv121a/inc/camsensor_gc0311.h
@@ -4,15 +4,15 @@
 #include "camsensor.h"
 #include "camctrl.h"
 
-boolean camsensor_gc0311_init
+boolean camsensor_GC0311_init
 (
 	camsensor_function_table_type *camsensor_function_table_ptr,
 	camctrl_tbl_type			  *camctrl_tbl_ptr
 );
 
 
-uint8 camsensor_gc0311_ycbcr_active(void);
-uint8 camsensor_gc0311_ycbcr_unactive(void);
+uint8 camsensor_GC0311_ycbcr_active(void);
+uint8 camsensor_GC0311_ycbcr_unactive(void);
 
 #endif
 
