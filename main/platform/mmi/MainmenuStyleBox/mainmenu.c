8c3b9257eb489b737511083370ecb3c921c8680f
xuhui <xuhui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2012-11-21 10:08:06 +0000

    git-svn-id: svn://127.0.0.1@5771 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/MainmenuStyleBox/mainmenu.c
+++ main/platform/mmi/MainmenuStyleBox/mainmenu.c
@@ -4533,15 +4533,15 @@ static int StartApplet(MainMenu *pMe, int i)
         break;
         
     case IDS_MAIN_WAPBROWSER:
-#ifdef FEATURE_VERSION_C337
 #if(defined(FEATURE_OEMOMH) 
         extern void OEM_SetBROWSER_ADSAccount(void);
         OEM_SetBROWSER_ADSAccount();
-#endif
+#endif		
+        #ifdef FEATURE_VERSION_C337
         Result = ISHELL_StartAppletArgs(pMe->m_pShell, AEECLSID_UCWEB, (char*)"call_ucweb:setmainpageurl:http://mimicromax.com");
-#else
+        #else
         Result = ISHELL_StartApplet(pMe->m_pShell, AEECLSID_UCWEB);
-#endif
+        #endif
         break;
     
     case IDS_MAIN_MENU_CALCULATOR: 
