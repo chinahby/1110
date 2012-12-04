36fe9896666d0b3705d12ecdd67241bae772f582
xuhui <xuhui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2012-12-04 09:48:26 +0000

    git-svn-id: svn://127.0.0.1@5871 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/asic/apps/StaticExtensions/OEM/Inc/oemnvint.h
+++ main/asic/apps/StaticExtensions/OEM/Inc/oemnvint.h
@@ -841,13 +841,13 @@ typedef struct _OEMErrLogType {
 #define OEMNV_EMERG_NUM_ONE                    "*1"
 #define OEMNV_EMERG_NUM_TWO                    "911"
 #define OEMNV_EMERG_NUM_TRE                    "*171"
-/*#elif defined(FEATURE_VERSION_C337)
+#elif defined(FEATURE_VERSION_C337)
 #define OEMNV_EMERT_SEZE                       4
 #define OEMNV_EMERG_NUM_LEN                    3
 #define OEMNV_EMERG_NUM_ONE                    "100"
 #define OEMNV_EMERG_NUM_TWO                    "101"
 #define OEMNV_EMERG_NUM_TRE                    "102"
-#define OEMNV_EMERG_NUM_FOR                    "108"*/
+#define OEMNV_EMERG_NUM_FOR                    "108"
 #else
 #define OEMNV_EMERT_SEZE                       4
 #define OEMNV_EMERG_NUM_LEN                    3
