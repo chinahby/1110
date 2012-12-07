8fd91818459528bc90d0c320447d4331d1455214
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2012-12-07 06:39:15 +0000

    git-svn-id: svn://127.0.0.1@5891 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/multimedia/camera/sensordriver/camsensor_siv121a/src/camsensor_sp0a18.c
+++ main/multimedia/camera/sensordriver/camsensor_siv121a/src/camsensor_sp0a18.c
@@ -64,12 +64,13 @@ $Header: //depot/asic/msm6550/drivers/camsensor/camsensor_ SP0A18_ycbcr.c#3 $ $D
 #define SP0A18_MAX_CONTRAST   4
 #define SP0A18_MIN_CONTRAST   0
 
+//’Î∂‘∫·∆¡¥Ú—…„œÒÕ∑
 /* From the logic analyzer measurements */
-#define OV_SP0A18_YCBCR_FULL_SIZE_WIDTH           640//640
-#define OV_SP0A18_YCBCR_FULL_SIZE_HEIGHT          480
+#define OV_SP0A18_YCBCR_FULL_SIZE_WIDTH           360//640//640
+#define OV_SP0A18_YCBCR_FULL_SIZE_HEIGHT          480//480
 
-#define OV_SP0A18_YCBCR_QTR_SIZE_WIDTH   		480//640   
-#define OV_SP0A18_YCBCR_QTR_SIZE_HEIGHT  		360//240//   
+#define OV_SP0A18_YCBCR_QTR_SIZE_WIDTH   		360//640   
+#define OV_SP0A18_YCBCR_QTR_SIZE_HEIGHT  		480//240//   
 
 /* Strobe Flash Epoch Interrupt time before the end of line count */
 
