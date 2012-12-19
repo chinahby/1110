b3637e94c3b3f49f50f95d6b7838b8bf01cc8f26
pengyuangui <pengyuangui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2012-12-19 07:43:58 +0000

    git-svn-id: svn://127.0.0.1@5970 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/multimedia/audio/avs/src/voccal.c
+++ main/multimedia/audio/avs/src/voccal.c
@@ -10504,6 +10504,11 @@ CAL_MEMORY voc_pcm_path_cal_type voc_pcm_on_chip_0_cal = {
   0x6000,                        /* CODEC Rx gain           */
                        /* CODEC Rx gain           */
 //  #endif
+#elif defined(FEATURE_VERSION_W317A)
+  0x956F, 					     /* Tx Voice Volume		   */
+  0x2000, 					     /* Tx DTMF gain			  */
+  0xCB00, 					     /* CODEC Tx gain 		  */
+  0x6000, 					     /* CODEC Rx gain 		  */
 #else
   0x4000,                        /* Tx Voice Volume         */
   0x1000,                        /* Tx DTMF gain            */
@@ -15090,7 +15095,12 @@ CAL_MEMORY voc_pcm_path_cal_type voc_pcm_on_chip_speaker_cal = {
   0x1000,						   /* Tx DTMF gain			*/
   0x5000,						   /* CODEC Tx gain			*/
   0x5000,						   /* CODEC Rx gain			*/
-#endif  
+#endif
+#elif defined(FEATURE_VERSION_W317A)
+  0x7000, 						   /* Tx Voice Volume 	  */
+  0x1000, 						   /* Tx DTMF gain		  */
+  0x5000, 						   /* CODEC Tx gain		  */
+  0x5000, 						   /* CODEC Rx gain		  */
 #else
   0x4000,                          /* Tx Voice Volume         */
   0x1000,                          /* Tx DTMF gain            */
