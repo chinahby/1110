ee2b5af2cc84973f91d02a1369a22cbb98c11a4c
pengyuangui <pengyuangui@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2013-01-05 10:08:04 +0000

    git-svn-id: svn://127.0.0.1@6150 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CoreApp/CoreApp_priv.h
+++ main/platform/mmi/CoreApp/CoreApp_priv.h
@@ -385,9 +385,9 @@
 #define    PWROFF_ANI_FRAME_COUNT                PWRON_ANI_FRAME_COUNT
 #define    PWROFF_ANI_RATE                       PWRON_ANI_RATE
 #elif defined FEATURE_VERSION_C180
-#define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweronoff.png"
+#define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweronoff.gif"
 #define    PWRON_ANI_FRAME_COUNT                 (1)
-#define    PWRON_ANI_RATE                        (4000)
+#define    PWRON_ANI_RATE                        ( 8600)
 #define    PWROFF_ANI_FILE                       PWRON_ANI_FILE
 #define    PWROFF_ANI_FRAME_COUNT                (1)
 #define    PWROFF_ANI_RATE                       (4000) 
