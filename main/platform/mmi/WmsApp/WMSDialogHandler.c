3034489bb2bc1329deafe68473e629a5568507c9
xxuezhen <xxuezhen@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2013-04-26 06:58:27 +0000

    修改K202短信默认输入法
    git-svn-id: svn://127.0.0.1@6458 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/WmsApp/WMSDialogHandler.c
+++ main/platform/mmi/WmsApp/WMSDialogHandler.c
@@ -11456,21 +11456,9 @@ static boolean IDD_WRITEMSG_Handler(void *pUser,
 		         	{
 		         		(void)ITEXTCTL_SetInputMode(pIText, AEE_TM_LETTERS);
 		         	}
-		         	}
-		         	#elif defined(FEATURE_LANG_CHINESE) //xxzhen for FEATURE_VERSION_K202_LM129C
-		         	{
-		         	nv_language_enum_type language;
-        	    	OEM_GetConfig( CFGI_LANGUAGE_SELECTION,&language,sizeof(language));
-                    if(NV_LANGUAGE_CHINESE == language)
-                    {
-		         		(void)ITEXTCTL_SetInputMode(pIText, AEE_TM_PINYIN);
-		         	}
-		         	else
-		         	{
-		         		(void)ITEXTCTL_SetInputMode(pIText, AEE_TM_LETTERS);
-		         	}
-		         	}
-		         	
+		         	}		         	
+		         	#elif defined(FEATURE_VERSION_K202_LM129C)//xxzhen
+					(void)ITEXTCTL_SetInputMode(pIText, AEE_TM_PINYIN);
 		         	#else
 					#ifdef FEATURE_ALL_KEY_PAD
         	    	(void)ITEXTCTL_SetInputMode(pIText, AEE_TM_RAPID);
