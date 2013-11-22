4525da99b40fc284741c8a77f974190034a88f45
wenyu <wenyu@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2013-11-22 06:47:21 +0000

    1.K272 图片资源 2.K272 屏
    git-svn-id: svn://127.0.0.1@7208 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/multimedia/display/lcd/src/disp_drv_220x176.c
+++ main/multimedia/display/lcd/src/disp_drv_220x176.c
@@ -39,6 +39,7 @@ extern boolean disp_HX8340b_tft20_rot(disp_drv_ic_type *pdispic);
 extern boolean disp_ili9225g_tft20_rot(disp_drv_ic_type *pdispic);
 extern boolean disp_rm68130_tft20_rot(disp_drv_ic_type *pdispic);
 extern boolean disp_st7775r_tft20_rot(disp_drv_ic_type *pdispic);
+extern boolean disp_nv3038_tft20_rot(disp_drv_ic_type *pdispic);
 
 
 /*============================================================================
@@ -51,6 +52,7 @@ static disp_ic_install_type     disp_ic_install[] =
 {
     disp_ili9225b_tft20_rot,//默认驱动，需放到最后，并且不检查IC的ID    
     disp_rm68130_tft20_rot,
+    disp_nv3038_tft20_rot,
     disp_st7775r_tft20_rot,
     disp_HX8340b_tft20_rot,
     NULL
