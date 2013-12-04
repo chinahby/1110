198f3fa4832d5ea4b2f07d9d06adaaa2f5f5c1a1
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2013-12-04 01:57:10 +0000

    git-svn-id: svn://127.0.0.1@7279 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/brew/brewpk/pk/src/oemitextctl.c
+++ main/platform/brew/brewpk/pk/src/oemitextctl.c
@@ -4099,7 +4099,6 @@ static void TextCtl_ShowSymbolPage(CTextCtl * pme, int nDir)
            // szbuf[1] = (AECHAR)'\0';
            // AECHAR szbuf[] = {'E', 'n', 't', 'e', 'r', '\0'};
            // IDISPLAY_DrawText(pd, AEE_FONT_NORMAL, szbuf, -1, rc.x+10, y, NULL,IDF_TEXT_TRANSPARENT/*0*/);
-           	  #ifdef FEATURE_USES_MMS
               IImage *enterImg; 
               enterImg = NULL;   
               enterImg = ISHELL_LoadResImage(pme->m_pIShell,
@@ -4111,13 +4110,11 @@ static void TextCtl_ShowSymbolPage(CTextCtl * pme, int nDir)
                   IIMAGE_Release(enterImg);
                   enterImg = NULL;
               } 
-			  #endif
          }
          else if(*pszSym == ' ')
          {
             //AECHAR szbuf[] = {'S', 'P','\0'};
             //IDISPLAY_DrawText(pd, AEE_FONT_NORMAL, szbuf, -1, rc.x+10, y, NULL,IDF_TEXT_TRANSPARENT/*0*/);
-            #ifdef FEATURE_USES_MMS
              IImage *spaceImg; 
               spaceImg = NULL;   
               spaceImg = ISHELL_LoadResImage(pme->m_pIShell,
@@ -4129,7 +4126,6 @@ static void TextCtl_ShowSymbolPage(CTextCtl * pme, int nDir)
                   IIMAGE_Release(spaceImg);
                   spaceImg = NULL;
               } 
-			 #endif
          }
          else
          {
