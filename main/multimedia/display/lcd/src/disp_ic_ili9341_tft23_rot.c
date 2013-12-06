b1ab93af8b8e0f6bde8ba7b69cb825171fa6488f
yangdecai <yangdecai@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2013-12-06 06:47:59 +0000

    jst [瀚彩屏]
    git-svn-id: svn://127.0.0.1@7298 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/multimedia/display/lcd/src/disp_ic_ili9341_tft23_rot.c
+++ main/multimedia/display/lcd/src/disp_ic_ili9341_tft23_rot.c
@@ -73,116 +73,110 @@ static void disp_ic_mdp_scrupdate(uint32 *scr, uint32 start_row, uint32 start_co
 //int reg=0xAA;
 static void disp_ic_init(void)
 {
-    /*
-    LCD_DELAY(120);       // Delay 120 ms 
-     
-    // ************* Start Initial Sequence ********** // 
-    LCD_WRITE_CMD(0xCF);  
-    LCD_WRITE_DATA(0x00); 
-    LCD_WRITE_DATA(0xC1); 
-    LCD_WRITE_DATA(0X30); 
-     
-    LCD_WRITE_CMD(0xED);  
-    LCD_WRITE_DATA(0x64); 
-    LCD_WRITE_DATA(0x03); 
-    LCD_WRITE_DATA(0X12); 
-    LCD_WRITE_DATA(0X81); 
-     
-    LCD_WRITE_CMD(0xE8);  
-    LCD_WRITE_DATA(0x85); 
-    LCD_WRITE_DATA(0x00); 
-    LCD_WRITE_DATA(0x78); 
-     
-    LCD_WRITE_CMD(0xCB);  
-    LCD_WRITE_DATA(0x39); 
-    LCD_WRITE_DATA(0x2C); 
-    LCD_WRITE_DATA(0x00); 
-    LCD_WRITE_DATA(0x34); 
-    LCD_WRITE_DATA(0x02); 
-     
-    LCD_WRITE_CMD(0xF7);  
-    LCD_WRITE_DATA(0x20); 
-     
-    LCD_WRITE_CMD(0xEA);  
-    LCD_WRITE_DATA(0x00); 
-    LCD_WRITE_DATA(0x00); 
-     
-    LCD_WRITE_CMD(0xC0);    //Power control 
-    LCD_WRITE_DATA(0x1F);   //VRH[5:0] 18
-     
-    LCD_WRITE_CMD(0xC1);    //Power control 
-    LCD_WRITE_DATA(0x13);   //SAP[2:0];BT[3:0] 
-     
-    LCD_WRITE_CMD(0xC5);    //VCM control 
-    LCD_WRITE_DATA(0x32); 
-    LCD_WRITE_DATA(0x3C); 
-     
-    LCD_WRITE_CMD(0xC7);    //VCM control2 
-    LCD_WRITE_DATA(0X92); 
-     
-    LCD_WRITE_CMD(0x36);    // Memory Access Control 
-    LCD_WRITE_DATA(0x08); 
-     
-    LCD_WRITE_CMD(0x3A);   
-    LCD_WRITE_DATA(0x55); 
-    
-    LCD_WRITE_CMD(0xB1);   
-    LCD_WRITE_DATA(0x00);   
-    LCD_WRITE_DATA(0x18); 
-     
-    LCD_WRITE_CMD(0xB6);    // Display Function Control 
-    LCD_WRITE_DATA(0x0A); 
-    LCD_WRITE_DATA(0xA2); 
-    
-    LCD_WRITE_CMD(0xF2);    // 3Gamma Function Disable 
-    LCD_WRITE_DATA(0x00); 
-     
-    LCD_WRITE_CMD(0x26);    //Gamma curve selected 
-    LCD_WRITE_DATA(0x01); 
-     
-    LCD_WRITE_CMD(0xE0);    //Set Gamma 
-    LCD_WRITE_DATA(0x0F); 
-    LCD_WRITE_DATA(0x1D); 
-    LCD_WRITE_DATA(0x1A); 
-    LCD_WRITE_DATA(0x0A); 
-    LCD_WRITE_DATA(0x0D); 
-    LCD_WRITE_DATA(0x07); 
-    LCD_WRITE_DATA(0x49); 
-    LCD_WRITE_DATA(0X66); 
-    LCD_WRITE_DATA(0x3B); 
-    LCD_WRITE_DATA(0x07); 
-    LCD_WRITE_DATA(0x11); 
-    LCD_WRITE_DATA(0x01); 
-    LCD_WRITE_DATA(0x09); 
-    LCD_WRITE_DATA(0x05); 
-    LCD_WRITE_DATA(0x04); 
-     
-    LCD_WRITE_CMD(0XE1);    //Set Gamma 
-    LCD_WRITE_DATA(0x00); 
-    LCD_WRITE_DATA(0x18); 
-    LCD_WRITE_DATA(0x1D); 
-    LCD_WRITE_DATA(0x02); 
-    LCD_WRITE_DATA(0x0F); 
-    LCD_WRITE_DATA(0x04); 
-    LCD_WRITE_DATA(0x36); 
-    LCD_WRITE_DATA(0x13); 
-    LCD_WRITE_DATA(0x4C); 
-    LCD_WRITE_DATA(0x07); 
-    LCD_WRITE_DATA(0x13); 
-    LCD_WRITE_DATA(0x0F); 
-    LCD_WRITE_DATA(0x2E); 
-    LCD_WRITE_DATA(0x2F); 
-    LCD_WRITE_DATA(0x05); 
-     
-    LCD_WRITE_CMD(0x11);    //Exit Sleep 
-    LCD_DELAY(120); 
-    LCD_WRITE_CMD(0x29);    //Display on 
-    */
+  
     
     //--************ Start Initial Sequence **********--/
  #if defined(FEATURE_VERSION_K212_HUALU)
  	LCD_DELAY(120);      // Delay 120 ms 
- 
+ 	#ifdef FEATURE_VERSION_K232_JST    //å«²Ê 9341v
+    LCD_WRITE_CMD(0xEF);  
+	LCD_WRITE_DATA(0x03); 
+	LCD_WRITE_DATA(0x80); 
+	LCD_WRITE_DATA(0x02); 
+	
+	LCD_WRITE_CMD(0xCF);  
+	LCD_WRITE_DATA(0x00); 
+	LCD_WRITE_DATA(0xC1); //0x81
+	LCD_WRITE_DATA(0X30); 
+	
+	LCD_WRITE_CMD(0xED);  
+	LCD_WRITE_DATA(0x67); 
+	LCD_WRITE_DATA(0x03); 
+	LCD_WRITE_DATA(0X12); 
+	LCD_WRITE_DATA(0X81); 
+	
+	LCD_WRITE_CMD(0xE8);  
+	LCD_WRITE_DATA(0x85); 
+	LCD_WRITE_DATA(0x00); 
+	LCD_WRITE_DATA(0x79); 
+	
+	LCD_WRITE_CMD(0xCB);  
+	LCD_WRITE_DATA(0x39); 
+	LCD_WRITE_DATA(0x2C); 
+	LCD_WRITE_DATA(0x00); 
+	LCD_WRITE_DATA(0x34); 
+	LCD_WRITE_DATA(0x02); 
+	
+	LCD_WRITE_CMD(0xF7);  
+	LCD_WRITE_DATA(0x20); 
+	
+	LCD_WRITE_CMD(0xEA);  
+	LCD_WRITE_DATA(0x00); 
+	LCD_WRITE_DATA(0x00); 
+	
+	LCD_WRITE_CMD(0xC0);       //Power control 
+	LCD_WRITE_DATA(0x23);     //VRH[5:0] 
+	
+	LCD_WRITE_CMD(0xC1);       //Power control 
+	LCD_WRITE_DATA(0x12);     //SAP[2:0];BT[3:0] 
+	
+	LCD_WRITE_CMD(0xC5);       //VCM control 
+	LCD_WRITE_DATA(0x30); 
+	LCD_WRITE_DATA(0x3F); 
+	
+	LCD_WRITE_CMD(0xC7);       //VCM control2 
+	LCD_WRITE_DATA(0x92); 
+	
+	LCD_WRITE_CMD(0x36);       // Memory Access Control 
+	LCD_WRITE_DATA(0x48); 
+	
+	LCD_WRITE_CMD(0xB1);       //VCM control 
+	LCD_WRITE_DATA(0x00); 
+	LCD_WRITE_DATA(0x10); 
+
+    LCD_WRITE_CMD(0x3A);   
+	LCD_WRITE_DATA(0x55); 
+	
+	LCD_WRITE_CMD(0xF2);       // 3Gamma Function Disable 
+	LCD_WRITE_DATA(0x00); 
+	
+	LCD_WRITE_CMD(0x26);       //Gamma curve selected 
+	LCD_WRITE_DATA(0x01); 
+	
+	LCD_WRITE_CMD(0xE0);       //Set Gamma 
+	LCD_WRITE_DATA(0x0F); 
+	LCD_WRITE_DATA(0x22); 
+	LCD_WRITE_DATA(0x1C); 
+	LCD_WRITE_DATA(0x1B); 
+	LCD_WRITE_DATA(0x08); 
+	LCD_WRITE_DATA(0x0F); 
+	LCD_WRITE_DATA(0x48); 
+	LCD_WRITE_DATA(0xB8); 
+	LCD_WRITE_DATA(0x34); 
+	LCD_WRITE_DATA(0x05); 
+	LCD_WRITE_DATA(0x0C); 
+	LCD_WRITE_DATA(0x09); 
+	LCD_WRITE_DATA(0x0F); 
+	LCD_WRITE_DATA(0x07); 
+	LCD_WRITE_DATA(0x00); 
+	
+	LCD_WRITE_CMD(0XE1);       //Set Gamma 
+	LCD_WRITE_DATA(0x00); 
+	LCD_WRITE_DATA(0x23); 
+	LCD_WRITE_DATA(0x24); 
+	LCD_WRITE_DATA(0x07); 
+	LCD_WRITE_DATA(0x10); 
+	LCD_WRITE_DATA(0x07); 
+	LCD_WRITE_DATA(0x38); 
+	LCD_WRITE_DATA(0x47); 
+	LCD_WRITE_DATA(0x4B); 
+	LCD_WRITE_DATA(0x0A); 
+	LCD_WRITE_DATA(0x13); 
+	LCD_WRITE_DATA(0x06); 
+	LCD_WRITE_DATA(0x30); 
+	LCD_WRITE_DATA(0x38); 
+	LCD_WRITE_DATA(0x0F); 
+    #else
 	//************* Start Initial Sequence **********// 
 	LCD_WRITE_CMD(0xCF);  
 	LCD_WRITE_DATA(0x00); 
@@ -284,6 +278,7 @@ static void disp_ic_init(void)
 	LCD_WRITE_DATA(0x2E); 
 	LCD_WRITE_DATA(0x3C); 
 	LCD_WRITE_DATA(0x0F); 
+    #endif
  #elif defined(FEATURE_VERSION_K212)
     LCD_DELAY(120);       // Delay 120 ms 
  
