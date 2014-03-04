58fe19f2fe5e8fb500ca1a79caad50cce3e342cb
yfangzhong <yfangzhong@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2014-03-04 07:32:28 +0000

    K292 万胜丰 M10C版本
    git-svn-id: svn://127.0.0.1@7667 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- main/platform/mmi/CoreApp/CoreApp_priv.h
+++ main/platform/mmi/CoreApp/CoreApp_priv.h
@@ -710,12 +710,12 @@ typedef enum
 #define    PWROFF_ANI_FRAME_COUNT                PWRON_ANI_FRAME_COUNT
 #define    PWROFF_ANI_RATE                       PWRON_ANI_RATE
 #elif defined FEATURE_VERSION_K292_WSF_M10C
-#define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweronoff.gif"
+#define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweron.gif"
 #define    PWRON_ANI_FRAME_COUNT                 (1)
-#define    PWRON_ANI_RATE                        (7100)
-#define    PWROFF_ANI_FILE                       PWRON_ANI_FILE
+#define    PWRON_ANI_RATE                        (15500)
+#define    PWROFF_ANI_FILE                       "fs:/image/pwronoffani/poweroff.gif"
 #define    PWROFF_ANI_FRAME_COUNT                PWRON_ANI_FRAME_COUNT
-#define    PWROFF_ANI_RATE                       PWRON_ANI_RATE
+#define    PWROFF_ANI_RATE                       (15000)
 #elif defined FEATURE_VERSION_K292_WSF_CN
 #define    PWRON_ANI_FILE                        "fs:/image/pwronoffani/poweron.gif"
 #define    PWRON_ANI_FRAME_COUNT                 (1)
