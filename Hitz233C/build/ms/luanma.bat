130cedfa14b90fe3a08ce434718c3fc6f1da3a8c
VisualSVN Server <VisualSVN Server@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2014-03-26 08:51:53 +0000

    Deleted folder '/Hitz233C'.
    git-svn-id: svn://127.0.0.1@7838 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- Hitz233C/build/ms/luanma.bat
+++
@@ -1,2 +0,0 @@
-rem 如果编译过程显示乱码，运行这个批处理
-set OUTPUT_CHARSET=gbk
--- Hitz233C/modem/uim/mmgsdi/src/mmgsdi_sap.h
+++
@@ -1,4 +0,0 @@
-#if (defined(FEATURE_BT) || defined(FEATURE_WM_BT)) && defined (FEATURE_BT_EXTPF_SAP)
-#error code not present
-#endif /* (FEATURE_BT || FEATURE_WM_BT) && FEATURE_BT_EXTPF_SAP */
-
