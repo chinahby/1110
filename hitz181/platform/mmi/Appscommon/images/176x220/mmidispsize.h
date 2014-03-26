eeb4bf464110d0bd3b70358644215197822befe4
VisualSVN Server <VisualSVN Server@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2014-03-26 08:52:04 +0000

    Deleted folder '/hitz181'.
    git-svn-id: svn://127.0.0.1@7841 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- hitz181/platform/mmi/Appscommon/images/176x220/mmidispsize.h
+++
@@ -1,4 +0,0 @@
-#ifndef MMIDISPSIZE_H_
-#define MMIDISPSIZE_H_
-#define FEATURE_DISP_176X220
-#endif //MMIDISPSIZE_H_
--- hitz181/tools/headergen/createnorprogimg.cmd
+++
@@ -1,1 +0,0 @@
-@norprogimgtool secelf_test/amss.mbn secelf_test/qcsblhd_cfgdata.mbn secelf_test/qcsbl.mbn secelf_test/oemsbl.mbn secelf_test/oemsblhd.mbn secelf_test/all.mbn
