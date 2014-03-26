596f4138ae84fa2512d252118cc237e1211e6c3f
VisualSVN Server <VisualSVN Server@3f3bbce5-bc85-8142-9eb1-77d090cfebd4>
2014-03-26 08:51:08 +0000

    Deleted folder '/WCDMASuite'.
    git-svn-id: svn://127.0.0.1@7829 3f3bbce5-bc85-8142-9eb1-77d090cfebd4

--- WCDMASuite/CHangupCmdPacket.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include ".\CHangupCmdPacket.h"
-#include <string>
-
-CHangupCmdPacket::CHangupCmdPacket()
-{
-	std::string cmd = "AT+CHUP\r";
-	SetMessage(cmd.c_str(), cmd.size());
-}
--- WCDMASuite/CHangupCmdPacket.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "CMobilePacket.h"
-
-class CHangupCmdPacket: public CMobilePacket
-{
-public:
-	CHangupCmdPacket();
-};
